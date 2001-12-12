9922e66f2fa97ea2f90c65ea1af146bebb549227
lothar <lothar>
2001-12-12 17:41:54 +0000

    Initial

---
+++ Test/Console/XML/export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export LBXMLLIB=lbDOMConfig.so                 
+export MODULELIB=lbModule.so                   
+
+export LBMODULEFUNCTOR=getlb_ModuleInstance__FPP12lb_I_UnknownP11lb_I_ModulePci
+export LBXMLFUNCTOR=getlbDOMConfigInstance__FPP12lb_I_UnknownP11lb_I_ModulePci
+
+export LBHOSTCFGFILE=/home/lothar/Develop/Projects/CPP/AppDevelopment/Console/XML/lbxmlconfig.xml
