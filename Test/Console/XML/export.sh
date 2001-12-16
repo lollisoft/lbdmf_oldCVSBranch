bafd3a707b25a03a76af81cb61e79c4786701265
lothar <lothar>
2001-12-16 17:48:09 +0000

    Corrected functors

--- Test/Console/XML/export.sh
+++ Test/Console/XML/export.sh
@@ -3,7 +3,9 @@
 export LBXMLLIB=lbDOMConfig.so                 
 export MODULELIB=lbModule.so                   
 
-export LBMODULEFUNCTOR=getlb_ModuleInstance__FPP12lb_I_UnknownP11lb_I_ModulePci
+#export LBMODULEFUNCTOR=getlb_ModuleInstance__FPP12lb_I_UnknownP11lb_I_ModulePci
 export LBXMLFUNCTOR=getlbDOMConfigInstance__FPP12lb_I_UnknownP11lb_I_ModulePci
+#export LBXMLFUNCTOR=getlbDOMConfigInstance
+export LBMODULEFUNCTOR=getlb_ModuleInstance
 
 export LBHOSTCFGFILE=/home/lothar/Develop/Projects/CPP/AppDevelopment/Console/XML/lbxmlconfig.xml
