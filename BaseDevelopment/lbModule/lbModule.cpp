a698abfa4605bd75a30d9b0aca24bda56408c180
lothar <lothar>
2002-04-25 18:07:57 +0000

    Added Skiplist implementation

--- BaseDevelopment/lbModule/lbModule.cpp
+++ BaseDevelopment/lbModule/lbModule.cpp
@@ -3,11 +3,14 @@
 /*...sRevision history:0:*/
 /**************************************************************
  * $Locker:  $
- * $Revision: 1.25 $
+ * $Revision: 1.26 $
  * $Name:  $
- * $Id: lbModule.cpp,v 1.25 2002/04/24 18:36:19 lothar Exp $
+ * $Id: lbModule.cpp,v 1.26 2002/04/25 18:07:57 lothar Exp $
  *
  * $Log: lbModule.cpp,v $
+ * Revision 1.26  2002/04/25 18:07:57  lothar
+ * Added Skiplist implementation
+ *
  * Revision 1.25  2002/04/24 18:36:19  lothar
  * Dump references and print references
  *
@@ -89,6 +92,467 @@ extern "C" {
 #include <lbConfigHook.h>
 #include <lbkey.h>
 /*...e*/
+
+class lbSkipListElement;
+typedef lb_I_Element* Elem;
+#define MAXLEVEL 9
+
+
+/*...sclass SkipNode:0:*/
+class SkipNode {
+  public:
+    int myLevel;
+    UAP(lb_I_Element, value, __FILE__, __LINE__)
+    SkipNode** forward;
+    
+    SkipNode();
+    SkipNode(lb_I_Element* r, int level);
+    virtual ~SkipNode();
+    
+};
+/*...e*/
+/*...sclass SkipList:0:*/
+class SkipList : public lb_I_Container {
+private:
+      SkipNode* head;
+      SkipNode* skipiterator;
+      int level;
+      int flag;
+
+      void AdjustHead(int& level) {level = MAXLEVEL;}
+
+public:
+	SkipList();
+	virtual ~SkipList();
+	
+	
+	DECLARE_LB_UNKNOWN()
+	DECLARE_LB_I_CONTAINER_IMPL()
+
+public:
+
+
+	lb_I_Unknown* search(lb_I_KeyBase*);
+	void insert(Elem);
+      
+      
+      
+	void remove(Elem);
+	int can_dump();
+	Elem dump_next();
+};
+/*...e*/
+/*...sclass lbSkipListElement:0:*/
+class lbSkipListElement : public lb_I_Element {
+public:
+    lbSkipListElement() { 
+    	ref = STARTREF; 
+    	next = NULL; 
+    	data = NULL; 
+    	key = NULL; 
+    	manager = NULL;
+    }
+    virtual ~lbSkipListElement();
+	
+    lbSkipListElement(const lb_I_Element &e) { 
+    	ref = STARTREF; 
+    	next = e.getNext(); 
+	manager = NULL;
+    }
+
+    DECLARE_LB_UNKNOWN()
+
+    DECLARE_LB_ELEMENT(lbSkipListElement)
+#ifndef UNIX
+    lb_I_Unknown* getObject() const;
+#endif
+};
+/*...e*/
+
+/*...sclass lbSkipListContainer:0:*/
+/*...sSkipNode implementation:0:*/
+SkipNode::SkipNode() {
+    	myLevel = MAXLEVEL;
+    	value = NULL;
+    	forward = new SkipNode* [myLevel+1];
+    	for (int i=0; i<=myLevel; i++)
+    	    forward[i] = NULL;
+}
+    
+SkipNode::SkipNode(lb_I_Element* r, int level) {
+        myLevel = level;
+        value = r;
+        forward = new SkipNode* [level+1];
+        for (int i=0; i<=level; i++)
+            forward[i] = NULL;
+}
+SkipNode::~SkipNode() { 
+      LOG("SkipNode destructor called")
+      delete [] forward; 
+      LOG("Deleted forward array")
+      
+      if (value != NULL) RELEASE(value)
+      LOG("Released value of this element")
+}
+/*...e*/
+
+BEGIN_IMPLEMENT_LB_UNKNOWN(SkipList)
+	ADD_INTERFACE(lb_I_Container)
+END_IMPLEMENT_LB_UNKNOWN()
+
+
+lbErrCodes LB_STDCALL SkipList::setData(lb_I_Unknown* uk) {
+	CL_LOG("SkipList::setData(...) not implemented yet");
+	return ERR_NOT_IMPLEMENTED;
+}
+SkipList::SkipList() {
+	iteration = 0;
+	head = new SkipNode();
+	skipiterator = NULL;
+	flag = 1;
+	level = MAXLEVEL;
+}
+
+SkipList::~SkipList() {
+	delete head;
+}
+/*...sSkipList\58\\58\Count\40\\41\:0:*/
+int LB_STDCALL SkipList::Count() { 
+        return count; 
+} 
+/*...e*/
+/*...sSkipList\58\\58\deleteAll\40\\41\:0:*/
+void LB_STDCALL SkipList::deleteAll() { 
+	LOG("DeleteAll() called")
+	delete head;
+	LOG("Deleted head")
+	head = new SkipNode();
+	LOG("New head created")
+	level = MAXLEVEL;
+
+	iteration = 0;
+	skipiterator = NULL;
+} 
+/*...e*/
+/*...sSkipList\58\\58\exists\40\lb_I_KeyBase\42\\42\ const key\41\:0:*/
+int LB_STDCALL SkipList::exists(lb_I_KeyBase** const key) { 
+    lb_I_Unknown* s = search(*key);
+    
+    if (s == NULL) return 0;	
+
+    return 1; 
+} 
+/*...e*/
+/*...sSkipList\58\\58\insert\40\lb_I_Unknown\42\\42\ const e\44\ lb_I_KeyBase\42\\42\ const key\41\:0:*/
+lbErrCodes LB_STDCALL SkipList::insert(lb_I_Unknown** const e, lb_I_KeyBase** const key) { 
+        lbErrCodes err = ERR_NONE; 
+        
+        lbSkipListElement* el = new lbSkipListElement(*e, *key);
+
+        insert(el);
+        
+        return err; 
+} 
+/*...e*/
+/*...sSkipList\58\\58\remove\40\lb_I_KeyBase\42\\42\ const key\41\:0:*/
+lbErrCodes LB_STDCALL SkipList::remove(lb_I_KeyBase** const key) { 
+        lbErrCodes err = ERR_NONE; 
+        lbSkipListElement* el = new lbSkipListElement(NULL, *key);
+        
+        remove(el);
+        
+        delete el;
+
+        return err; 
+} 
+/*...e*/
+/*...sSkipList\58\\58\_insert\40\lb_I_Unknown\42\\42\ const e\44\ lb_I_KeyBase\42\\42\ const key\41\:0:*/
+lbErrCodes LB_STDCALL SkipList::_insert(lb_I_Unknown** const e, lb_I_KeyBase** const key) { 
+/*...sbla:0:*/
+#ifdef bla
+    if (container_data == NULL) { 
+        lbElement* _data = new lbElement(*e, *key); 
+        _data->setModuleManager(manager.getPtr(), __FILE__, __LINE__); 
+
+        _data->queryInterface("lb_I_Element", (void**) &container_data, __FILE__, __LINE__); 
+        if (container_data == NULL) CL_LOG("Could not get unknown interface of lbElement!"); 
+
+        lb_I_Unknown* uk_o = NULL; 
+        if ((uk_o = container_data->getObject()) == NULL) { 
+                CL_LOG("Failed to insert first element in SkipList::insert"); 
+                return ERR_CONTAINER_INSERT; 
+        } else RELEASE(uk_o); 
+    } 
+    else { 
+        lb_I_Element* temp; 
+        for (temp = container_data; temp != NULL; temp = temp->getNext()) { 
+            lb_I_Element* next = temp->getNext(); 
+
+            if (next != NULL) { 
+                if (next->getKey() < *key) { 
+                    lbElement* el = new lbElement(*e, *key, next); 
+                    el->setModuleManager(manager.getPtr(), __FILE__, __LINE__); 
+                    temp->setNext(el); 
+                    return ERR_NONE; 
+                } 
+            } 
+            else { 
+            	lbElement* el = new lbElement(*e, *key, next); 
+            	el->setModuleManager(manager.getPtr(), __FILE__, __LINE__); 
+                temp->setNext(el); 
+                return ERR_NONE; 
+            } 
+        } 
+    } 
+#endif    
+/*...e*/
+    return ERR_NONE; 
+} 
+/*...e*/
+/*...sSkipList\58\\58\_remove\40\lb_I_KeyBase\42\\42\ const key\41\:0:*/
+lbErrCodes LB_STDCALL SkipList::_remove(lb_I_KeyBase** const key) { 
+/*...sbla:0:*/
+#ifdef bla
+    if (container_data == NULL) { 
+        CL_LOG("Error: Can't remove from empty container!"); 
+        return ERR_CONTAINER_REMOVE; 
+    } 
+
+    lb_I_Element* pre = container_data; 
+    container_data = container_data->getNext(); 
+
+    if (pre->getKey() == *key) { 
+        RELEASE(pre); 
+        return ERR_NONE; 
+    } 
+
+    for (lb_I_Element* temp = container_data; temp != NULL; temp = container_data->getNext()) { 
+        if (temp->getKey() == *key) { 
+            pre->setNext(temp->getNext()); 
+            RELEASE(temp); 
+            return ERR_NONE; 
+        } 
+    } 
+#endif
+    CL_LOG("Error: No object with that key"); 
+/*...e*/
+    return ERR_CONTAINER_REMOVE; 
+} 
+/*...e*/
+/*...sSkipList\58\\58\hasMoreElements\40\\41\:0:*/
+int LB_STDCALL SkipList::hasMoreElements() { 
+    return can_dump();
+} 
+/*...e*/
+/*...sSkipList\58\\58\nextElement\40\\41\:0:*/
+lb_I_Unknown* LB_STDCALL SkipList::nextElement() { 
+
+	Elem e = dump_next();
+	
+	if(e != NULL) {
+		return e->getObject();
+	} else {
+	        CL_LOG("Error: Please call hasMoreElements first to check if any elements are available!"); 
+        	getch(); 
+	        return NULL; 
+	}
+} 
+/*...e*/
+/*...sSkipList\58\\58\getElement\40\lb_I_KeyBase\42\\42\ const key\41\:0:*/
+lb_I_Unknown* LB_STDCALL SkipList::getElement(lb_I_KeyBase** const key) { 
+
+    return search(*key);
+} 
+/*...e*/
+/*...sSkipList\58\\58\setElement\40\lb_I_KeyBase\42\\42\ key\44\ lb_I_Unknown \42\\42\ const e\41\:0:*/
+void LB_STDCALL SkipList::setElement(lb_I_KeyBase** key, lb_I_Unknown ** const e) { 
+    remove(key); 
+    insert(e, key); 
+}
+/*...e*/
+
+
+/*...srandomLevel\40\void\41\:0:*/
+int randomLevel(void) { // Pick a level on exponential distribution
+  int level;
+  for (level=0; (rand()%2) == 0; level++); // Do nothing
+  return level;
+}
+/*...e*/
+/*...sSkipList\58\\58\search\40\lb_I_KeyBase\42\ searchKey\41\:0:*/
+lb_I_Unknown* SkipList::search(lb_I_KeyBase* searchKey) { // Skiplist Search
+  SkipNode *x = head;                  // Dummy header node
+  
+  if (x == NULL) LOG("Error: NULL pointer while searching in skiplist");
+  
+  for (int i=level; i>=0; i--) {
+    while ((x->forward[i] != NULL) && (*(x->forward[i]->value) < searchKey)) {
+      x = x->forward[i];
+    }
+  }
+  x = x->forward[0];  // Move to actual record, if it exists
+  if ((x != NULL) && (*(x->value) == searchKey)) {
+  	return x->value->getObject();
+  } else return NULL;
+}
+/*...e*/
+/*...sSkipList\58\\58\insert\40\Elem newValue\41\:0:*/
+void SkipList::insert(Elem newValue) { // Insert into skiplist
+  SkipNode *x = head;           // Start at header node
+  int i;
+  int newLevel = randomLevel(); // Select level for new node
+
+  if (newLevel > level) {       // New node will be deepest in list
+    AdjustHead(newLevel);       // Add null pointers to header
+    level = newLevel;
+  }
+	  
+  SkipNode** update = NULL;    // Update tracks end of each level
+  
+  update = new SkipNode* [level+1]; 
+
+  for(i=level; i>=0; i--) { // Search for insert position
+    lb_I_Element* e = (x->forward[i] != NULL) ? x->forward[i]->value.getPtr() : NULL;
+    while((x->forward[i] != NULL) && (*e < newValue->getKey())) {
+      x = x->forward[i];
+      e = x->forward[i] != NULL ? x->forward[i]->value.getPtr() : NULL;
+    }
+    update[i] = x;              // Keep track of end at level i
+  }
+  
+  x = new SkipNode(newValue, newLevel); // Create new node
+  
+  for (i=0; i<=newLevel; i++) { // Splice into list
+    x->forward[i] = update[i]->forward[i]; // Who x points to
+    update[i]->forward[i] = x;             // Who y points to
+  }
+}
+/*...e*/
+/*...sSkipList\58\\58\remove\40\Elem searchKey\41\:0:*/
+void SkipList::remove(Elem searchKey) {
+    // update holds pointers to next elements of each level
+    SkipNode** update = NULL;    // Update tracks end of each level
+  
+    update = new SkipNode* [level+1]; 
+    
+    SkipNode *x = head;                  // Dummy header node
+    SkipNode *found = NULL;
+    
+    for (int i=level; i>=0; i--) {
+
+      while ((x->forward[i] != NULL) && (*(x->forward[i]->value) < searchKey->getKey())) {
+        x = x->forward[i];
+      }
+        
+      update[i] = x;
+    }
+
+    x = x->forward[0];  // Move to actual record, if it exists
+    
+    if ((x != NULL) && (*(x->value) == searchKey->getKey())) found = x;
+    else return;
+
+    // element found, so rebuild list without node:
+    if (*(found->value) == searchKey->getKey()) {
+      for (int i=0; i<=level; i++) {
+        if (update[i]->forward[i] == found) {
+          update[i]->forward[i] = found->forward[i];
+        }
+      }
+      
+      // element can be freed now (would happen automatically):
+      delete found;
+      
+      // maybe we have to downcorrect the level of the list: 
+      while (level>0  &&  head->forward[level] != NULL && head->forward[level]->value==NULL) {
+        level--;
+      }
+    }
+  }
+
+
+/*...e*/
+/*...sSkipList\58\\58\can_dump\40\\41\:0:*/
+int SkipList::can_dump() {
+	if (iteration == 0) { 
+        	iteration = 1; 
+	        skipiterator = head; 
+		flag = 1;
+	} 
+
+	if (skipiterator == NULL) { 
+	        iteration = 0; 
+	        flag = 1;
+	        return 0; 
+	} 
+	
+//	if (flag == 0) return 0;
+	
+	return 1;
+}
+/*...e*/
+/*...sSkipList\58\\58\dump_next\40\\41\:0:*/
+Elem SkipList::dump_next() {
+	if (skipiterator != NULL) {
+		Elem e = skipiterator->value.getPtr();
+		
+		while (e == NULL) {
+			skipiterator = skipiterator->forward[0];
+			e = skipiterator->value.getPtr();
+		}
+
+		skipiterator = skipiterator->forward[0];
+/*...sbla:0:*/
+#ifdef bla		
+		for(int i=0; i<=skipiterator->myLevel && flag != 0; i++)
+			if (skipiterator->forward[i] == NULL){
+				printf("No more elements in skiplist\n");
+		        	flag = 0;
+			}
+#endif		
+/*...e*/
+		return e;
+	} else {
+		printf("Return NULL because skipiterator is NULL\n");
+		return NULL;
+	}
+}
+
+/*...sbla:0:*/
+#ifdef bla
+void dump() {
+    SkipNode* temp = head;
+    int flag = 1;
+
+    for ( ; temp!= NULL; temp = temp->forward[0]) {
+        cout << "temp->value is " << temp->value << endl;
+        for(int i=0; i<=temp->myLevel && flag != 0; i++)
+            if (temp->forward[i] == NULL){
+                cout << " rest of list is empty" << endl;
+                flag = 0;
+            }
+            else
+                cout<<"  point to "<<temp->forward[i]->value<<"\n";
+    }
+}
+#endif
+/*...e*/
+/*...e*/
+
+BEGIN_IMPLEMENT_LB_UNKNOWN(lbSkipListElement)
+        ADD_INTERFACE(lb_I_Element)
+END_IMPLEMENT_LB_UNKNOWN()
+
+IMPLEMENT_LB_ELEMENT(lbSkipListElement)
+
+lbErrCodes LB_STDCALL lbSkipListElement::setData(lb_I_Unknown* uk) {
+	CL_LOG("lbSkipListElement::setData(...) not implemented yet");
+	return ERR_NOT_IMPLEMENTED;
+}
+/*...e*/
+
+
+
 /*...sclass InstanceRepository:0:*/
 /*...sreferenceList:0:*/
 typedef struct _referenceList {
@@ -272,12 +736,15 @@ char* LB_STDCALL lbInstanceReference::getFile() {
 }
 
 void LB_STDCALL lbInstanceReference::setLine(int l) {
+	line = l;
 }
 
 void LB_STDCALL lbInstanceReference::setCount(int c) {
+	count = c;
 }
 
 void LB_STDCALL lbInstanceReference::setFile(char* f) {
+
 }
 	
 lbErrCodes LB_STDCALL lbInstanceReference::setData(lb_I_Unknown* uk) {
@@ -685,9 +1152,9 @@ void LB_STDCALL InstanceRepository::loadContainer(lb_I_Module* m) {
 	if (loadedContainer == 1) return;
 	loadedContainer = 1;
 
-printf("LoadContainer for instance repository\n");
+printf("Load container for instance repository\n");
 	STATIC_REQUEST(m, lb_I_Container, instlist)
-	
+printf("Got an instance\n");	
 	lb_iList = instlist;
 	
 	instanceList* temp = iList;
