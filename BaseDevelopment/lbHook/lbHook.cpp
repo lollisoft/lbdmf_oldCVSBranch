f32c263c8cfbb4fc3ced5eb00ae117be2d5b9945
lothar <lothar>
2002-06-03 05:25:36 +0000

    Added MSC pragmas

--- BaseDevelopment/lbHook/lbHook.cpp
+++ BaseDevelopment/lbHook/lbHook.cpp
@@ -22,6 +22,11 @@ extern "C" {
 
 #endif
 
+#ifdef _MSC_VER
+#pragma warning( disable: 4275 )
+#pragma warning( disable: 4251 )
+#pragma warning( disable: 4101 )
+#endif
 
 #include <lbConfigHook.h>
 #include <lbKey.h>
