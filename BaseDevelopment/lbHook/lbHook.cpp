1a4bb26ef54eeb31320804feef3f7c9256744419
lothar <lothar>
2002-08-31 10:50:28 +0000

    Removed a printf and decreased char buffer allocation

--- BaseDevelopment/lbHook/lbHook.cpp
+++ BaseDevelopment/lbHook/lbHook.cpp
@@ -44,9 +44,8 @@ DLLEXPORT void LB_STDCALL CL_doLog(char* f, char* msg) {
                 fp = fopen( f, "a" );
                 if( fp != NULL ) {
                         char *buf = NULL;
-                        buf = (char*) malloc(10000);
+                        buf = (char*) malloc(500);
                         buf[0] = 0;
-                        printf( "%s%s", buf, msg);
                         fprintf( fp, "%s%s", buf, msg);
                         free((void*) buf);
                 }
