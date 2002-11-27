1312f5fb4be600d7ea6bb4d659386e5ea332a9bf
lothar <lothar>
2002-11-27 18:45:52 +0000

    Initial

---
+++ BaseDevelopment/lbHook/makefile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This script creates a makefile based on templates
+
+cat $DEVROOT/Projects/CPP/make/makefile.template > makefile
+cat makefile.module >> makefile
+echo all: lbhook >> makefile
+
+mkmk so lbhook ../../vendor/dosdir/*.c *.cpp >> makefile
