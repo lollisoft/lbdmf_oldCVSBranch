9a481f2fed66c8a3e0964c11a701ea34adfa4aca
lollisoft <lollisoft>
2003-05-29 10:40:17 +0000

    Bugfix because linking fails on readonly backup files

--- BaseDevelopment/lbDB/makefile.bat
+++ BaseDevelopment/lbDB/makefile.bat
@@ -1,6 +1,9 @@
 @echo off
 @rem This script creates a makefile based on templates
 
+rem Linking fails, if the backup file of the source is readonly
+attrib -r *.bak
+
 type %DEVROOT%\Projects\cpp\make\makefile.template > makefile
 type makefile.module >> makefile
 echo Path=$(PATH) >> makefile
