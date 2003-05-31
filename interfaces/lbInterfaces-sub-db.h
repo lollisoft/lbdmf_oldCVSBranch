6ca4c81839bf1db0f2883a154360eb30fbcf88db
lollisoft <lollisoft>
2003-05-31 16:37:04 +0000

    Begin design of database interfaces

--- interfaces/lbInterfaces-sub-db.h
+++ interfaces/lbInterfaces-sub-db.h
@@ -1,6 +1,18 @@
 #ifndef __LB_DATABASE__
 #define __LB_DATABASE__
 
+class lb_I_Query : public lb_I_Unknown
+{
+	lb_I_Query() {}
+	virtual ~lb_I_Query() {}
+	
+	virtual lbErrCodes LB_STDCALL query(char* q) = 0;
+	virtual lbErrCodes LB_STDCALL first() = 0;
+	virtual lbErrCodes LB_STDCALL next() = 0;
+	virtual lbErrCodes LB_STDCALL previous() = 0;
+	virtual lbErrCodes LB_STDCALL last() = 0;
+};
+
 class lb_I_Database : public lb_I_Unknown
 {
 protected:
@@ -15,5 +27,9 @@ public:
 	 * trainres and the table is as the following definition:
 	 */
 	virtual lbErrCodes LB_STDCALL connect(char* DSN, char* user, char* passwd) = 0;
+	
+	virtual lb_I_Query LB_STDCALL getQuery() = 0;
+	
+	
 };
 #endif // __LB_DATABASE__
