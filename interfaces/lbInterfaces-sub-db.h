f1125ecd753ca101dc987603e8537a975746baf2
lollisoft <lollisoft>
2003-06-03 21:46:34 +0000

    More interface attempts

--- interfaces/lbInterfaces-sub-db.h
+++ interfaces/lbInterfaces-sub-db.h
@@ -1,17 +1,56 @@
 #ifndef __LB_DATABASE__
 #define __LB_DATABASE__
 
+/*
+ * A single bound column, such as a text field on a form
+ */
+class lb_I_BoundColumn : public lb_I_Unknown
+{
+protected:
+        lb_I_BoundColumn() {}
+        virtual ~lb_I_BoundColumn() {}
+public:
+	virtual char* getDataType() = 0;
+};
+
+/*
+ * This interface is an attempt to providing column binding
+ * informations for the query.
+ */
+class lb_I_ColumnBinding : public lb_I_Unknown
+{
+protected:
+        lb_I_ColumnBinding() {}
+        virtual ~lb_I_ColumnBinding() {}
+public:
+	/*------ Data transver variants ------------*/
+        /* Data is available, I can read it out ... */
+        virtual lbErrCodes LB_STDCALL dataAvailable() = 0;
+        virtual lbErrCodes LB_STDCALL setQueryInstance(lb_I_Query* q);
+        
+        /* Get bound columns. If the ColumnBinding instance does not
+         * bind all colums of a query, it would provide dummies.
+         */
+        virtual lb_I_Container* LB_STDCALL getBoundColumns() = 0;
+        virtual lbErrCodes      LB_STDCALL setBoundColumns(lb_I_Container* bc) = 0;
+};
+
 class lb_I_Query : public lb_I_Unknown
 {
 protected:
-	lb_I_Query() {}
-	virtual ~lb_I_Query() {}
-public:	
-	virtual lbErrCodes LB_STDCALL query(char* q) = 0;
-	virtual lbErrCodes LB_STDCALL first() = 0;
-	virtual lbErrCodes LB_STDCALL next() = 0;
-	virtual lbErrCodes LB_STDCALL previous() = 0;
-	virtual lbErrCodes LB_STDCALL last() = 0;
+        lb_I_Query() {}
+        virtual ~lb_I_Query() {}
+public:
+
+        virtual lbErrCodes LB_STDCALL setView(lb_I_ColumnBinding* cb) = 0;
+
+        /* Set the SQL query */
+        virtual lbErrCodes LB_STDCALL query(char* q) = 0;
+        /* Navigation */
+        virtual lbErrCodes LB_STDCALL first() = 0;
+        virtual lbErrCodes LB_STDCALL next() = 0;
+        virtual lbErrCodes LB_STDCALL previous() = 0;
+        virtual lbErrCodes LB_STDCALL last() = 0;
 };
 
 class lb_I_Database : public lb_I_Unknown
