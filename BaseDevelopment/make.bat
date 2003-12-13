49b1e9e62e26bf43a4702e72c2f14fc7a643a8b6
lollisoft <lollisoft>
2003-12-13 11:06:40 +0000

    Make bak file writable. Anything tries to overwrite it and my editor makes backupfiles that stay readonly.

--- BaseDevelopment/make.bat
+++ BaseDevelopment/make.bat
@@ -1,39 +1,47 @@
 cd lbHook
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..
 
 cd lbDB
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..
 
 cd lbModule
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..
 
 cd lbMetaApplication
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..
 
 cd lbclasses
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..
 
 cd lbXMLConfig\lbDOMConfig
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..\..
 
 cd ..\test\Console\XML
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..\..\..\BaseDevelopment
 
 cd ..\test\GUI\wxWrapper
+attrib -r *.bak
 call makefile.bat
 make --win32
 cd ..\..\..\BaseDevelopment
