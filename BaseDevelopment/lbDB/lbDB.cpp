84c6b7a550d34f79106118315ae80659c00e6d0b
lollisoft <lollisoft>
2003-12-16 21:12:56 +0000

    Try to use SQLFetchScroll, included some docs for dynamically allocated buffer. But does not help.

--- BaseDevelopment/lbDB/lbDB.cpp
+++ BaseDevelopment/lbDB/lbDB.cpp
@@ -749,7 +749,9 @@ lbErrCodes LB_STDCALL lbQuery::first() {
         // Indicate, that data must prebound to a buffer
         databound = 0;
 
-        retcode = SQLExtendedFetch(hstmt, SQL_FETCH_FIRST, 0, &RowsFetched, RowStat);
+//        retcode = SQLExtendedFetch(hstmt, SQL_FETCH_FIRST, 0, &RowsFetched, RowStat);
+
+	retcode = SQLFetchScroll(hstmt, SQL_FETCH_FIRST, 0);
 
         if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO) {
                 _LOG << "lbQuery::first(): Error while fetching next row" LOG_
@@ -762,7 +764,7 @@ lbErrCodes LB_STDCALL lbQuery::first() {
                 return ERR_DB_FETCHFIRST;
         }
         
-        if (RowsFetched == 0) return ERR_DB_FETCHFIRST;
+//        if (RowsFetched == 0) return ERR_DB_FETCHFIRST;
         
 	return ERR_NONE;
 }
@@ -775,7 +777,9 @@ lbErrCodes LB_STDCALL lbQuery::next() {
 	// Indicate, that data must prebound to a buffer
 	databound = 0;
 
-	retcode = SQLExtendedFetch(hstmt, SQL_FETCH_NEXT, 0, &RowsFetched, RowStat);
+	//retcode = SQLExtendedFetch(hstmt, SQL_FETCH_NEXT, 0, &RowsFetched, RowStat);
+
+	retcode = SQLFetchScroll(hstmt, SQL_FETCH_NEXT, 0);
 
 	if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO) {
 		_LOG << "lbQuery::next(): Error while fetching next row" LOG_
@@ -788,7 +792,7 @@ lbErrCodes LB_STDCALL lbQuery::next() {
 		return ERR_DB_NODATA;
         }
 
-	if (RowsFetched == 0) return ERR_DB_NODATA;
+//	if (RowsFetched == 0) return ERR_DB_NODATA;
 
 	return ERR_NONE;
 }
@@ -801,7 +805,9 @@ lbErrCodes LB_STDCALL lbQuery::previous() {
         // Indicate, that data must prebound to a buffer
         databound = 0;
 
-        retcode = SQLExtendedFetch(hstmt, SQL_FETCH_PREV, 0, &RowsFetched, RowStat);
+        //retcode = SQLExtendedFetch(hstmt, SQL_FETCH_PREV, 0, &RowsFetched, RowStat);
+
+	retcode = SQLFetchScroll(hstmt, SQL_FETCH_PREV, 0);
 
         if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO) {
                 _LOG << "lbQuery::previous(): Error while fetching next row" LOG_
@@ -814,7 +820,7 @@ lbErrCodes LB_STDCALL lbQuery::previous() {
                 return ERR_DB_NODATA;
         }
         
-	if (RowsFetched == 0) return ERR_DB_NODATA;
+//	if (RowsFetched == 0) return ERR_DB_NODATA;
         
 	return ERR_NONE;
 }
@@ -827,7 +833,9 @@ lbErrCodes LB_STDCALL lbQuery::last() {
         // Indicate, that data must prebound to a buffer
         databound = 0;
 
-        retcode = SQLExtendedFetch(hstmt, SQL_FETCH_LAST, 0, &RowsFetched, RowStat);
+        //retcode = SQLExtendedFetch(hstmt, SQL_FETCH_LAST, 0, &RowsFetched, RowStat);
+
+	retcode = SQLFetchScroll(hstmt, SQL_FETCH_LAST, 0);
 
         if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO) {
                 _LOG << "lbQuery::last(): Error while fetching next row" LOG_
@@ -841,7 +849,7 @@ lbErrCodes LB_STDCALL lbQuery::last() {
                 return ERR_DB_FETCHLAST;
         }
         
-        if (RowsFetched == 0) return ERR_DB_FETCHLAST;
+//        if (RowsFetched == 0) return ERR_DB_FETCHLAST;
                       
 	return ERR_NONE;
 }
@@ -1149,8 +1157,7 @@ lbErrCodes LB_STDCALL lbBoundColumn::setFromString(lb_I_String* set, int mode) {
 	return ERR_NONE;
 }
 /*...e*/
-/*...slbErrCodes LB_STDC
-			}ALL lbBoundColumn\58\\58\bindColumn\40\lbQuery\42\ q\44\ int column\41\:0:*/
+/*...slbErrCodes LB_STDCALL lbBoundColumn\58\\58\bindColumn\40\lbQuery\42\ q\44\ int column\41\:0:*/
 lbErrCodes LB_STDCALL lbBoundColumn::bindColumn(lbQuery* q, int column) {
 
 	HSTMT hstmt = q->getCurrentStatement();
@@ -1197,9 +1204,11 @@ lbErrCodes LB_STDCALL lbBoundColumn::bindColumn(lbQuery* q, int column) {
 			buffersize = (ColumnSize == 0) ? (BufferLength+1)*rows : (ColumnSize+1)*rows;
 			if (buffersize == 0) printf("Fatal: Calculated bufferzize is zero!!\n");
 			
-			buffer = malloc(buffersize);
+			static char staticBuffer[1000];
 			
-			printf("Buffer pointer is at %p\n", buffer);
+			buffer = &staticBuffer; //malloc(buffersize);
+			
+			printf("Buffer pointer is at %p with size %d\n", buffer, buffersize);
 			
 			
 			_DataType = DataType;
@@ -1250,6 +1259,108 @@ lbErrCodes LB_STDCALL lbBoundColumn::bindColumn(lbQuery* q, int column) {
 
 	return ERR_NONE;
 }
+/*...sSample of dynamically allocated buffer:0:*/
+#ifdef bla
+
+// I cannot use that because I have a separate class of a bound column.
+// And why should it impossible to allocate separate buffers for each
+// column.
+
+
+// This application allocates a buffer at run time. For each column, this buffer
+// contains memory for the column's data and length/indicator. For example:
+//
+//  column 1    column 2 column 3 column 4
+// <------------><---------------><-----><------------>
+//  db1 li1  db2   li2 db3 li3  db4 li4
+//   |   |    |   | | |  |   |
+//  _____V_____V________V_______V___V___V______V_____V_
+// |__________|__|_____________|__|___|__|__________|__|
+//
+// dbn = data buffer for column n
+// lin = length/indicator buffer for column n
+
+// Define a macro to increase the size of a buffer so it is a multiple of the alignment
+// size. Thus, if a buffer starts on an alignment boundary, it will end just before the
+// next alignment boundary. In this example, an alignment size of 4 is used because
+// this is the size of the largest data type used in the application's buffer -- the
+// size of an SDWORD and of the largest default C data type are both 4. If a larger
+// data type (such as _int64) was used, it would be necessary to align for that size.
+#define ALIGNSIZE 4
+#define ALIGNBUF(Length) Length % ALIGNSIZE ? \
+         Length + ALIGNSIZE - (Length % ALIGNSIZE) : Length
+
+SQLCHAR   SelectStmt[100];
+SQLSMALLINT NumCols, *CTypeArray, i;
+SQLINTEGER  *ColLenArray, *OffsetArray, SQLType, *DataPtr;
+SQLRETURN rc; 
+SQLHSTMT  hstmt;
+
+// Get a SELECT statement from the user and execute it.
+GetSelectStmt(SelectStmt, 100);
+SQLExecDirect(hstmt, SelectStmt, SQL_NTS);
+
+// Determine the number of result set columns.  Allocate arrays to hold the C type,
+// byte length, and buffer offset to the data.
+SQLNumResultCols(hstmt, &NumCols);
+CTypeArray = (SQLSMALLINT *) malloc(NumCols * sizeof(SQLSMALLINT));
+ColLenArray = (SQLINTEGER *) malloc(NumCols * sizeof(SQLINTEGER));
+OffsetArray = (SQLINTEGER *) malloc(NumCols * sizeof(SQLINTEGER));
+
+OffsetArray[0] = 0;
+for (i = 0; i < NumCols; i++) {
+ // Determine the column's SQL type. GetDefaultCType contains a switch statement that
+ // returns the default C type for each SQL type.
+ SQLColAttribute(hstmt, ((SQLUSMALLINT) i) + 1, SQL_DESC_TYPE, NULL, 0, NULL, (SQLPOINTER) &SQLType);
+ CTypeArray[i] = GetDefaultCType(SQLType);
+
+ // Determine the column's byte length. Calculate the offset in the buffer to the
+ // data as the offset to the previous column, plus the byte length of the previous
+ // column, plus the byte length of the previous column's length/indicator buffer.
+ // Note that the byte length of the column and the length/indicator buffer are
+ // increased so that, assuming they start on an alignment boundary, they will end on
+ // the byte before the next alignment boundary. Although this might leave some holes
+ // in the buffer, it is a relatively inexpensive way to guarantee alignment.
+ SQLColAttribute(hstmt, ((SQLUSMALLINT) i)+1, SQL_DESC_OCTET_LENGTH, NULL, 0, NULL, &ColLenArray[i]);
+ ColLenArray[i] = ALIGNBUF(ColLenArray[i]);
+ if (i)
+  OffsetArray[i] = OffsetArray[i-1]+ColLenArray[i-1]+ALIGNBUF(sizeof(SQLINTEGER));
+}
+
+// Allocate the data buffer. The size of the buffer is equal to the offset to the data
+// buffer for the final column, plus the byte length of the data buffer and
+// length/indicator buffer for the last column.
+void *DataPtr = malloc(OffsetArray[NumCols - 1] +
+        ColLenArray[NumCols - 1] + ALIGNBUF(sizeof(SQLINTEGER)));
+
+// For each column, bind the address in the buffer at the start of the memory allocated
+// for that column's data and the address at the start of the memory allocated for that
+// column's length/indicator buffer.
+for (i = 0; i < NumCols; i++)
+ SQLBindCol(hstmt,
+      ((SQLUSMALLINT) i) + 1,
+      CTypeArray[i],
+      (SQLPOINTER)((SQLCHAR *)DataPtr + OffsetArray[i]),
+      ColLenArray[i],
+      (SQLINTEGER *)((SQLCHAR *)DataPtr + OffsetArray[i] + ColLenArray[i]));
+
+// Retrieve and print each row. PrintData accepts a pointer to the data, its C type,
+// and its byte length/indicator. It contains a switch statement that casts and prints
+// the data according to its type. Code to check if rc equals SQL_ERROR or
+// SQL_SUCCESS_WITH_INFO not shown.
+while ((rc = SQLFetch(hstmt)) != SQL_NO_DATA) {
+ for (i = 0; i < NumCols; i++) {
+  PrintData((SQLCHAR *)DataPtr[OffsetArray[i]], CTypeArray[i],
+      (SQLINTEGER *)((SQLCHAR *)DataPtr[OffsetArray[i] + ColLenArray[i]]));
+ }
+}
+
+// Close the cursor.
+SQLCloseCursor(hstmt);
+
+
+#endif
+/*...e*/
 /*...e*/
 lb_I_String* LB_STDCALL lbBoundColumn::getColumnName() {
 	return colName.getPtr();
