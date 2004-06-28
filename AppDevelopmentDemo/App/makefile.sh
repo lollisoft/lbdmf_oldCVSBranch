b3fed2d23376f32ebad83b02a34a1b68e7ad136f
lollisoft <lollisoft>
2004-06-28 19:31:55 +0000

    Initial

---
+++ AppDevelopmentDemo/App/makefile.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+# This script creates a makefile based on templates
+
+cat ../../make/makefile.template > makefile
+cat makefile.module >> makefile
+echo all: Application >> makefile
+echo >> makefile
+
+mkmk SO Application ../dosdir,../include,../interfaces *.cpp >> makefile
