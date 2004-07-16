1243d6e2174d45998a6db35609de88d6d3c986ab
lollisoft <lollisoft>
2004-07-16 20:35:52 +0000

    Copy all subdirectories and clean them from rubbish

--- make_dist_base.bat
+++ make_dist_base.bat
@@ -1,25 +1,25 @@
 
-xcopy /I /Y %1 %DEVROOT%\%1
+xcopy /E /I /Y %1 %DEVROOT%\%1
 cd %DEVROOT%\%1
-del *.exe
-del *.obj
-del *.bak
-del .#*
-del *.ilk
-del *.pch
-del *.pdb
-del *.lib
-del *.dll
-del *.mak
-del *.opt
-del *.lnk
-del *.lb1
-del *.lk1
-del *.mk1
-del *.sym
-del *.map
-del *.exp
-del *.idb
-del *.$$$
-del null
+del /S *.exe
+del /S *.obj
+del /S *.bak
+del /S .#*
+del /S *.ilk
+del /S *.pch
+del /S *.pdb
+del /S *.lib
+del /S *.dll
+del /S *.mak
+del /S *.opt
+del /S *.lnk
+del /S *.lb1
+del /S *.lk1
+del /S *.mk1
+del /S *.sym
+del /S *.map
+del /S *.exp
+del /S *.idb
+del /S *.$$$
+del /S null
 cd Q:\develop\Projects\CPP
