50a93202d6d1309ae2edcba611fc85d58a78cd5c
lollisoft <lollisoft>
2004-09-12 11:01:20 +0000

    Removed unused sample code and made a simpler fetch status return in remove method. This does not work either - no way how.

--- BaseDevelopment/lbDB/lbDB.cpp
+++ BaseDevelopment/lbDB/lbDB.cpp
@@ -933,6 +933,7 @@ int   LB_STDCALL lbQuery::getColumns() {
 	return cols;
 }
 
+/*...schar\42\ LB_STDCALL lbQuery\58\\58\getColumnName\40\int col\41\:0:*/
 char* LB_STDCALL lbQuery::getColumnName(int col) {
 	UAP(lb_I_BoundColumn, column, __FILE__, __LINE__)
 	column = boundColumns->getBoundColumn(col);
@@ -948,7 +949,7 @@ char* LB_STDCALL lbQuery::getColumnName(int col) {
 
 	return _name;
 }
-
+/*...e*/
 /*...slbErrCodes LB_STDCALL lbQuery\58\\58\first\40\\41\:0:*/
 lbErrCodes LB_STDCALL lbQuery::first() {
         UWORD   RowStat[20];
@@ -986,6 +987,7 @@ lbErrCodes LB_STDCALL lbQuery::first() {
 	return ERR_NONE;
 }
 /*...e*/
+/*...slbErrCodes LB_STDCALL lbQuery\58\\58\next\40\\41\:0:*/
 lbErrCodes LB_STDCALL lbQuery::next() {
 	UWORD   RowStat[20];
 	UDWORD  RowsFetched = 0;
@@ -1100,6 +1102,8 @@ _LOG << "Fetch gave no error, so all would be good." LOG_
 	return ERR_NONE;
 #endif
 }
+/*...e*/
+/*...slbErrCodes LB_STDCALL lbQuery\58\\58\previous\40\\41\:0:*/
 lbErrCodes LB_STDCALL lbQuery::previous() {
         UWORD   RowStat[20];
         UDWORD  RowsFetched = 0;
@@ -1199,6 +1203,7 @@ lbErrCodes LB_STDCALL lbQuery::previous() {
 	return ERR_NONE;
 #endif
 }
+/*...e*/
 /*...slbErrCodes LB_STDCALL lbQuery\58\\58\last\40\\41\:0:*/
 lbErrCodes LB_STDCALL lbQuery::last() {
         UWORD   RowStat[20];
@@ -1273,26 +1278,30 @@ UDWORD  RowsFetched = 0;
 
 	SQLSetPos(hstmt, 1, SQL_DELETE, SQL_LOCK_NO_CHANGE);
 
+	if (fetchstatus == 0) return next();
+	if (fetchstatus == 1) return previous();
+	return next();
+
+/*
 	if (fetchstatus == 0) {
 		lbErrCodes err = ERR_NONE;
 		err = next();
 		if ((err != ERR_NONE) && (err != WARN_DB_NODATA)) {
 			return first();
 		}
+		return err;
 	} else {		
 		switch (fetchstatus) {
 		
 		case 1  : 
 			return previous();
-			break;
 		case -1 :
 			return next();
-			break;
 		default:
 			break;
 		}
 	}
-
+*/
 	return ERR_NONE;
 }
 /*...e*/
@@ -1521,157 +1530,6 @@ void LB_STDCALL lbQuery::dbError(char* lp)
 	fprintf(stderr, "%s. %s, SQLSTATE=%s\n",lp, buf, sqlstate);
 }
 
-
-#ifdef bla
-/*...sbla:0:*/
-
-#define POSITIONED_UPDATE 100
-#define POSITIONED_DELETE 101
-
-SQLUINTEGER  CustIDArray[10];
-SQLCHAR	NameArray[10][51], 
-	AddressArray[10][51],
-	PhoneArray[10][11];
-
-SQLINTEGER
-	CustIDIndArray[10],
-	NameLenOrIndArray[10],
-	AddressLenOrIndArray[10],
-	PhoneLenOrIndArray[10];
-
-SQLUSMALLINT RowStatusArray[10], Action, RowNum;
-SQLHSTMT   hstmtCust, hstmtUpdate, hstmtDelete;
-
-
-
-
-
-
-
-// Set the SQL_ATTR_BIND_TYPE statement attribute to use column-wise binding. Declare
-// the rowset size with the SQL_ATTR_ROW_ARRAY_SIZE statement attribute. Set the
-// SQL_ATTR_ROW_STATUS_PTR statement attribute to point to the row status array.
-SQLSetStmtAttr(hstmtCust, SQL_ATTR_ROW_BIND_TYPE, SQL_BIND_BY_COLUMN, 0);
-SQLSetStmtAttr(hstmtCust, SQL_ATTR_ROW_ARRAY_SIZE, 10, 0);
-SQLSetStmtAttr(hstmtCust, SQL_ATTR_ROW_STATUS_PTR, RowStatusArray, 0);
-
-
-
-
-
-
-
-
-
-
-// Bind arrays to the CustID, Name, Address, and Phone columns.
-SQLBindCol(hstmtCust, 1, SQL_C_ULONG, CustIDArray, 0, CustIDIndArray);
-SQLBindCol(hstmtCust, 2, SQL_C_CHAR, NameArray, sizeof(NameArray[0]),
-     NameLenOrIndArray);
-SQLBindCol(hstmtCust, 3, SQL_C_CHAR, AddressArray, sizeof(AddressArray[0]),
-     AddressLenOrIndArray);
-SQLBindCol(hstmtCust, 4, SQL_C_CHAR, PhoneArray, sizeof(PhoneArray[0]),
-     PhoneLenOrIndArray);
-
-
-
-
-// Set the cursor name to Cust.
-SQLSetCursorName(hstmtCust, "Cust", SQL_NTS);
-
-
-
-
-
-
-// Prepare positioned update and delete statements.
-SQLPrepare(hstmtUpdate,
- "UPDATE Customers SET Address = ?, Phone = ? WHERE CURRENT OF Cust",
- SQL_NTS);
-SQLPrepare(hstmtDelete, "DELETE FROM Customers WHERE CURRENT OF Cust", SQL_NTS);
-
-
-
-
-
-
-
-// Execute a statement to retrieve rows from the Customers table.
-SQLExecDirect(hstmtCust,
- "SELECT CustID, Name, Address, Phone FROM Customers FOR UPDATE OF Address, Phone",
- SQL_NTS);
-
-
-
-
-
-
-
-// Fetch and display the first 10 rows.
-SQLFetchScroll(hstmtCust, SQL_FETCH_NEXT, 0);
-DisplayData(CustIDArray, CustIDIndArray, NameArray, NameLenOrIndArray, AddressArray,
-    AddressLenOrIndArray, PhoneArray, PhoneLenOrIndArray, RowStatusArray);
-
-
-
-
-
-
-// Call GetAction to get an action and a row number from the user.
-while (GetAction(&Action, &RowNum)) {
- switch (Action) {
-
-  case SQL_FETCH_NEXT:
-  case SQL_FETCH_PRIOR:
-  case SQL_FETCH_FIRST:
-  case SQL_FETCH_LAST:
-  case SQL_FETCH_ABSOLUTE:
-  case SQL_FETCH_RELATIVE:
-   // Fetch and display the requested data.
-   SQLFetchScroll(hstmtCust, Action, RowNum);
-   DisplayData(CustIDArray, CustIDIndArray, NameArray, NameLenOrIndArray,
-       AddressArray, AddressLenOrIndArray, PhoneArray,
-       PhoneLenOrIndArray, RowStatusArray);
-   break;
-
-  case POSITIONED_UPDATE:
-   // Get the new data and place it in the rowset buffers.
-   GetNewData(AddressArray[RowNum - 1], &AddressLenOrIndArray[RowNum - 1],
-        PhoneArray[RowNum - 1], &PhoneLenOrIndArray[RowNum - 1]);
-
-   // Bind the elements of the arrays at position RowNum-1 to the parameters
-   // of the positioned update statement.
-   SQLBindParameter(hstmtUpdate, 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR,
-          50, 0, AddressArray[RowNum - 1], sizeof(AddressArray[0]),
-          &AddressLenOrIndArray[RowNum - 1]);
-   SQLBindParameter(hstmtUpdate, 2, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR,
-          10, 0, PhoneArray[RowNum - 1], sizeof(PhoneArray[0]),
-          &PhoneLenOrIndArray[RowNum - 1]);
-
-   // Position the rowset cursor. The rowset is 1-based.
-   SQLSetPos(hstmtCust, RowNum, SQL_POSITION, SQL_LOCK_NO_CHANGE);
-
-   // Execute the positioned update statement to update the row.
-   SQLExecute(hstmtUpdate);
-   break;
-
-  case POSITIONED_DELETE:
-   // Position the rowset cursor. The rowset is 1-based.
-   SQLSetPos(hstmtCust, RowNum, SQL_POSITION, SQL_LOCK_NO_CHANGE);
-
-   // Execute the positioned delete statement to delete the row.
-   SQLExecute(hstmtDelete);
-   break;
- }
-}
-
-// Close the cursor.
-SQLCloseCursor(hstmtCust);
-
-
-
-/*...e*/
-#endif
 /*...e*/
 /*...sclass lbBoundColumn:0:*/
 
