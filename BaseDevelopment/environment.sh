25c8118f6a6536117bd2b2d8512aa8b05cd8624f
lollisoft <lollisoft>
2005-01-22 14:59:12 +0000

    Initial

---
+++ BaseDevelopment/environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export MODULELIB=lbModule.so
+export LBXMLFUNCTOR=getlbDOMConfigInstance
+export LBMODULEFUNCTOR=getlb_ModuleInstance
+export DEVROOT=~/develop
+export LD_LIBRARY_PATH=/libdev/lib:
+export OSTYPE=linux
+
