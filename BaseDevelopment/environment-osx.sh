74a779ebec398b09fe2ae32346beaaae5e5fbae8
lollisoft <lollisoft>
2005-02-10 12:54:03 +0000

    Added additional PATH

--- BaseDevelopment/environment-osx.sh
+++ BaseDevelopment/environment-osx.sh
@@ -6,3 +6,4 @@ export LBMODULEFUNCTOR=getlb_ModuleInstance
 export DEVROOT=~/develop
 export LD_LIBRARY_PATH=~/lib:
 export OSTYPE=osx
+export PATH="/sw/bin:/sw/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11R6/bin:~/bin"
