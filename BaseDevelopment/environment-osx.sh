1aabcefba8b44b221d0046d60a92be947e9d2c2a
lollisoft <lollisoft>
2005-02-10 12:51:58 +0000

    Added shell script for Mac OS X

---
+++ BaseDevelopment/environment-osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export MODULELIB=lbModule.so
+export LBXMLFUNCTOR=getlbDOMConfigInstance
+export LBMODULEFUNCTOR=getlb_ModuleInstance
+export DEVROOT=~/develop
+export LD_LIBRARY_PATH=~/lib:
+export OSTYPE=osx
