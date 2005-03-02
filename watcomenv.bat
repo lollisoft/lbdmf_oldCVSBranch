b89f23b0b86dc8942561d61afa960fade9e24988
lollisoft <lollisoft>
2005-03-02 19:59:57 +0000

    Changes due to be ablt to compile both - wx and my libraries without any changes in this file.

--- watcomenv.bat
+++ watcomenv.bat
@@ -31,18 +31,18 @@ goto BEGINENVIRONMENT:
 :DISTMODE
 
 set DEVLW=c:
-set BASE=lbDMF/develop
+set BASE=lbDMF\develop
 set BASE_MAKE=lbDMF/develop
 
 set BASE=lbDMF\develop
 
-set DEVROOT=%DEVLW%\%BASE%
+set DEVROOT=%DEVLW%/%BASE%
 set DEVROOT_MAKE=%DEVLW%/%BASE_MAKE%
 
 cd %DEVROOT%\Projects\dll
 if not exist "libs" mkdir libs
 cd %DEVROOT%\Projects\cpp\basedevelopment
-set BASE=lbDMF/develop
+set BASE=lbDMF\develop
 
 set DEVROOT=%DEVLW%/%BASE%
 
@@ -54,54 +54,55 @@ set DEVROOT=%DEVLW%/%BASE%
 @rem *                          End config                          *
 @rem ----------------------------------------------------------------
 
-set PLUGIN_DIR=%DEVROOT%/Projects/plugins
+rem WXWIN stuff
+
+set WXWIN=%DEVLW%\%BASE%\wxwin\wx
 
 
-set MSVCDir=%DEVROOT%/Tools/MSC/VC98
+set PLUGIN_DIR=%DEVLW%\%BASE%\Projects\plugins
+
+
+set MSVCDir=%DEVLW%\%BASE%\Tools\MSC\VC98
 
 @rem Alias to the linux environment
 set OSTYPE=%OS%
 
 rem Basic Runtime settings
-set RUNROOT=%DEVROOT%/projects
+set RUNROOT=%DEVLW%\%BASE%\projects
 
 rem Some bin dirs for development
 
-rem set CVSBIN=%DEVROOT%/Tools/cvs
-set WATBIN=%DEVROOT%/Tools/WATCOM/BINNT;%DEVROOT%/Tools/WATCOM/BINW
-set DEVBIN=%DEVROOT%/bin
+rem set CVSBIN=%DEVROOT%\Tools\cvs
+set WATBIN=%DEVLW%\%BASE%\Tools\WATCOM\BINNT;%DEVROOT%\Tools\WATCOM\BINW
+set DEVBIN=%DEVLW%\%BASE%\bin
 
 rem Some dll and bin search Paths
-set DLLROOT=%RUNROOT%/dll
-set BINROOT=%RUNROOT%/bin;%RUNROOT%/CPP/bin
+set DLLROOT=%RUNROOT%\dll
+set BINROOT=%RUNROOT%\bin;%RUNROOT%\CPP\bin
 
-set Path=%DEVLW%/%BASE%/bin;%SystemRoot%/system32;%DEVLW%/;%WATBIN%;%DEVBIN%;%DLLROOT%;%BINROOT%
-set Path=%Path%;q:/develop/tools/cygwin/bin;Q:/develop/Tools/Perl/bin
+set Path=%DEVLW%\%BASE%\bin;%SystemRoot%\system32;%DEVLW%\;%WATBIN%;%DEVBIN%;%DLLROOT%;%BINROOT%
+set Path=%Path%;q:\develop\tools\cygwin\bin;Q:\develop\Tools\Perl\bin
 
-set MSC=%MSVCDir%/Bin;%MSVCDir%/Lib
+set MSC=%MSVCDir%\Bin;%MSVCDir%\Lib
 
-set Path=%MSC%;%Path%;%DEVROOT%/bin
-rem ??? F:/develop/NT/DevTools/bin
+set Path=%MSC%;%Path%;%DEVLW%\%BASE%\bin
+rem ??? F:\develop\NT\DevTools\bin
 
 rem MSC Library path
-set LIB=%MSVCDir%/LIB;%MSVCDir%/MFC/LIB;%LIB%
+set LIB=%MSVCDir%\LIB;%MSVCDir%\MFC\LIB;%LIB%
 
 rem Watcom stuff
 
-SET INCLUDE=%DEVROOT%/wxwin/wx/src/msw;%MSVCDir%/Include;%DEVROOT%/Tools/WATCOM/
+SET INCLUDE=%DEVLW%\%BASE%\wxwin\wx\src\msw;%DEVLW%\%BASE%\Tools\WATCOM\;%MSVCDir%\Include
 
-rem ???;D:/Develop/Tools/WATCOM/H/NT
+rem ???;D:\Develop\Tools\WATCOM\H\NT
 rem SET INCLUDE=
-SET WATCOM=%DEVROOT%/Tools/WATCOM
-SET EDPath=%DEVROOT%/Tools/WATCOM/EDDAT
-
-rem WXWIN stuff
-
-set WXWIN=%DEVROOT%/wxwin/wx
+SET WATCOM=%DEVLW%\%BASE%\Tools\WATCOM
+SET EDPath=%DEVLW%\%BASE%\Tools\WATCOM\EDDAT
 
 @rem XML Module Configuration
 
-set LBHOSTCFGFILE=%DEVROOT%/Projects/CPP/Test/Console/XML/lbXMLConfig.xml
+set LBHOSTCFGFILE=%DEVLW%\%BASE%\Projects\CPP\Test\Console\XML\lbXMLConfig.xml
 
 REM __stdcall convention
 set LBXMLFUNCTOR=_getlbDOMConfigInstance@16
@@ -127,8 +128,8 @@ REM ------------------------------------------------------------
 REM This stuff is added due to integrate Doxygen into my console
 REM ------------------------------------------------------------
 
-set BISON_SIMPLE=%DEVLW%/cygwin/usr/share/bison.simple
-set path=%path%;%DEVLW%/develop/Tools/Perl/bin/
+set BISON_SIMPLE=%DEVLW%\cygwin\usr\share\bison.simple
+set path=%path%;%DEVLW%\develop\Tools\Perl\bin\
 
 start %1
 exit
