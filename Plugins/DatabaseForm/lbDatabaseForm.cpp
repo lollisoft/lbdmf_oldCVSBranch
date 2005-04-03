2478afbd3b6fa7e6a587f21d60793e257f5b05e4
lollisoft <lollisoft>
2005-04-03 22:12:48 +0000

    New dialog to help setup mapping of foreign keys to show more 'readable' data in the combo box.

--- Plugins/DatabaseForm/lbDatabaseForm.cpp
+++ Plugins/DatabaseForm/lbDatabaseForm.cpp
@@ -106,6 +106,380 @@ public:
 	DECLARE_PLUGINS()
 };
 
+/*...sclass lbConfigure_FK_PK_MappingDialog:0:*/
+/*...sclass lbConfigure_FK_PK_MappingDialog definition:0:*/
+class lbConfigure_FK_PK_MappingDialog :
+	public lb_I_EventHandler,
+	public lb_I_Unknown,
+	public wxDialog {
+public:
+	/**
+	 * Default constructor - implemented in BEGIN_IMPLEMENT_LB_UNKNOWN(lbDatabaseDialog)
+	 */
+	lbConfigure_FK_PK_MappingDialog();
+
+	/**
+	 * Destructor
+	 */
+	virtual ~lbConfigure_FK_PK_MappingDialog();
+
+	int prepareDialogHandler();
+
+	void LB_STDCALL init(lb_I_Query* query);
+
+	void LB_STDCALL show() { ShowModal (); };
+	void LB_STDCALL destroy() { Destroy(); };
+	
+	/**
+	 * Column has been selected.
+	 *
+	 * Store the retrieved data in the tables and destroy the form.
+	 */
+
+	lbErrCodes LB_STDCALL selectedColumn(lb_I_Unknown* uk);
+
+	/**
+	 * This function acts in a special way for registering the above navigation handlers
+	 *
+	 * It uses a string of the this pointer + a name for the respective eventhandler.
+	 * This is neccessary for handling more than one database dialog per application.
+	 *
+	 * This is a good sample, if you need to be able to handle more than one instance of
+	 * your registered event handlers.
+	 */
+	lbErrCodes LB_STDCALL registerEventHandler(lb_I_Dispatcher* dispatcher);
+
+	void OnDispatch(wxCommandEvent& event);
+
+	DECLARE_LB_UNKNOWN()
+
+
+	void OnFKComboBoxSelected( wxCommandEvent &event );
+	void OnPKComboBoxSelected( wxCommandEvent &event );
+	    
+/*...svariables:8:*/
+	UAP(lb_I_Database, database, __FILE__, __LINE__)
+	UAP(lb_I_Query, sampleQuery, __FILE__, __LINE__)
+	
+	/**
+	 * \brief Maps positions to id's for each displayed combo box.
+	 *
+	 * Store a container for each combo box with key(pos) and data(id). 
+	 */
+	UAP(lb_I_Container, ComboboxMapperList, __FILE__, __LINE__)
+	UAP(lb_I_Query, sourceQuery, __FILE__, __LINE__)
+	
+
+	// l gets overwritten, while assigning a lb_I_Query* pointer to sampleQuery !!
+	// l and buf are therefore as a bugfix.
+	long l;
+	char buf[100];
+	
+	wxWindow* firstButton;
+
+	wxComboBox* cBoxFKNames;
+	wxComboBox* cBoxPKNames;
+
+	wxStaticText *label;
+	wxStaticText *labelF;
+	
+	wxWindow* prevButton;
+	wxWindow* nextButton;
+	wxWindow* lastButton;
+/*...e*/
+};
+/*...e*/
+
+BEGIN_IMPLEMENT_LB_UNKNOWN(lbConfigure_FK_PK_MappingDialog)
+END_IMPLEMENT_LB_UNKNOWN()
+
+lbConfigure_FK_PK_MappingDialog::lbConfigure_FK_PK_MappingDialog() 
+: wxDialog(NULL, -1, wxString(_T("lbConfigure_FK_PK_MappingDialog dialog")), wxDefaultPosition,
+wxDefaultSize, wxRESIZE_BORDER|wxDEFAULT_DIALOG_STYLE)
+{
+	ref = STARTREF;
+}
+
+lbConfigure_FK_PK_MappingDialog::~lbConfigure_FK_PK_MappingDialog() {
+
+}
+
+lbErrCodes LB_STDCALL lbConfigure_FK_PK_MappingDialog::setData(lb_I_Unknown* uk) {
+        _CL_LOG << "lbConfigure_FK_PK_MappingDialog::setData(...) not implemented yet" LOG_
+
+        return ERR_NOT_IMPLEMENTED;
+}
+
+void lbConfigure_FK_PK_MappingDialog::OnFKComboBoxSelected( wxCommandEvent &event ) {
+	wxString s = cBoxFKNames->GetStringSelection();
+	
+	cBoxFKNames->Disable();
+	
+	REQUEST(manager.getPtr(), lb_I_Database, database)
+	
+	database->init();
+	
+	char* lbDMFPasswd = getenv("lbDMFPasswd");
+	char* lbDMFUser   = getenv("lbDMFUser");
+	
+	if (!lbDMFUser) lbDMFUser = "dba";
+	if (!lbDMFPasswd) lbDMFPasswd = "trainres";
+	
+	database->connect("lbDMF", lbDMFUser, lbDMFPasswd);
+	
+	char buf[] = "select * from %s";
+	char* buffer = (char*) malloc(strlen(buf)+s.Length()+1);
+	
+	UAP(lb_I_String, PKTable, __FILE__, __LINE__)
+	
+	char* p = strdup(s.c_str());
+	
+	PKTable = sourceQuery->getPKTable(p);
+	
+	free(p);
+	
+	sprintf(buffer, buf, PKTable->charrep());
+	
+	UAP(lb_I_Query, sampleQuery, __FILE__, __LINE__)
+	
+	sampleQuery = database->getQuery(0);
+
+	sampleQuery->query(buffer);
+	
+	for (int i = 1; i <= sampleQuery->getColumns(); i++) {
+		char* name = sampleQuery->getColumnName(i);
+		cBoxPKNames->Append(wxString(name));
+	}
+	
+	cBoxPKNames->Enable();
+}
+
+void lbConfigure_FK_PK_MappingDialog::OnPKComboBoxSelected( wxCommandEvent &event ) {
+	wxString PKName = cBoxPKNames->GetStringSelection();
+	wxString FKName = cBoxFKNames->GetStringSelection();
+
+	UAP(lb_I_String, PKTable, __FILE__, __LINE__)
+	
+	char* fkTable = strdup(sourceQuery->getTableName());
+	
+	char* p = strdup(FKName.c_str());
+	
+	PKTable = sourceQuery->getPKTable(p);
+	
+	free(p);
+	
+	cBoxPKNames->Disable();
+	
+	// Delete the entry, we now will put into the configuration
+	
+	cBoxFKNames->Delete(cBoxFKNames->GetSelection());
+	
+	
+	REQUEST(manager.getPtr(), lb_I_Database, database)
+	UAP(lb_I_Query, query, __FILE__, __LINE__)
+	
+	database->init();
+	
+	char* lbDMFPasswd = getenv("lbDMFPasswd");
+	char* lbDMFUser   = getenv("lbDMFUser");
+	
+	if (!lbDMFUser) lbDMFUser = "dba";
+	if (!lbDMFPasswd) lbDMFPasswd = "trainres";
+	
+	database->connect("lbDMF", lbDMFUser, lbDMFPasswd);
+	
+	char buf[] = "insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) values('%s','%s', '%s', '%s')";
+	char* buffer = (char*) malloc(	strlen(buf)+
+					PKName.Length()+
+					strlen(fkTable)+
+					FKName.Length()+
+					strlen(PKTable->charrep())+1);
+
+	buffer[0] = 0;
+
+	sprintf(buffer, buf, FKName.c_str(), fkTable, PKName.c_str(), PKTable->charrep());
+
+	printf("%s\n", buffer);
+	
+	query = database->getQuery(0);
+	
+	query->query(buffer);
+	
+	if (cBoxFKNames->GetCount() > 0) {
+		cBoxFKNames->SetSelection(-1);
+		cBoxFKNames->Enable();
+		cBoxPKNames->Clear();
+		cBoxPKNames->Disable();
+	} else {
+		cBoxPKNames->Disable();
+		cBoxFKNames->Disable();
+
+		firstButton->Enable();		
+	}
+	
+}
+
+lbErrCodes LB_STDCALL lbConfigure_FK_PK_MappingDialog::selectedColumn(lb_I_Unknown* uk) {
+	EndModal(wxID_OK);
+
+	return ERR_NONE;
+}
+
+int lbConfigure_FK_PK_MappingDialog::prepareDialogHandler() {
+	int SelectedColumn;
+	int cbFKSel;
+	int cbPKSel;
+	
+	UAP_REQUEST(manager.getPtr(), lb_I_EventManager, eman)
+	UAP_REQUEST(manager.getPtr(), lb_I_Dispatcher, dispatcher)
+
+	char eventName[100] = "";
+		
+	sprintf(eventName, "%pSelectedColumn", this);
+	eman->registerEvent(eventName, SelectedColumn);
+
+	dispatcher->setEventManager(eman.getPtr());
+
+	registerEventHandler(dispatcher.getPtr());
+
+
+	this->Connect( SelectedColumn,  -1, wxEVT_COMMAND_BUTTON_CLICKED, 
+		(wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction) 
+			&lbConfigure_FK_PK_MappingDialog::OnDispatch);
+
+	this->Connect( cBoxFKNames->GetId(),  -1, wxEVT_COMMAND_TEXT_UPDATED, 
+		(wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction) 
+			&lbConfigure_FK_PK_MappingDialog::OnFKComboBoxSelected);
+
+	this->Connect( cBoxPKNames->GetId(),  -1, wxEVT_COMMAND_TEXT_UPDATED, 
+		(wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction) 
+			&lbConfigure_FK_PK_MappingDialog::OnPKComboBoxSelected);
+
+	this->Connect( cBoxFKNames->GetId(),  -1, wxEVT_COMMAND_TEXT_ENTER, 
+		(wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction) 
+			&lbConfigure_FK_PK_MappingDialog::OnFKComboBoxSelected);
+
+	this->Connect( cBoxPKNames->GetId(),  -1, wxEVT_COMMAND_TEXT_ENTER, 
+		(wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction) 
+			&lbConfigure_FK_PK_MappingDialog::OnPKComboBoxSelected);
+
+	return SelectedColumn;
+}
+
+/*...slbErrCodes LB_STDCALL lbConfigure_FK_PK_MappingDialog\58\\58\registerEventHandler\40\lb_I_Dispatcher\42\ dispatcher\41\:0:*/
+lbErrCodes LB_STDCALL lbConfigure_FK_PK_MappingDialog::registerEventHandler(lb_I_Dispatcher* dispatcher) {
+	lbErrCodes err = ERR_NONE;
+
+	char eventName[100] = "";
+
+	sprintf(eventName, "%pSelectedColumn", this);
+	dispatcher->addEventHandlerFn(this, (lbEvHandler) &lbConfigure_FK_PK_MappingDialog::selectedColumn, eventName);
+
+	return err;
+}
+/*...e*/
+/*...svoid LB_STDCALL lbConfigure_FK_PK_MappingDialog\58\\58\init\40\lb_I_Query\42\ query\41\:0:*/
+void LB_STDCALL lbConfigure_FK_PK_MappingDialog::init(lb_I_Query* query) {
+	lbErrCodes err = ERR_NONE;
+	char prefix[100] = "";
+	sprintf(prefix, "%p", this);
+
+	QI(query, lb_I_Query, sourceQuery, __FILE__, __LINE__)
+	sourceQuery++;
+	
+/*...ssizers:0:*/
+	wxBoxSizer* sizerMain  = new wxBoxSizer(wxVERTICAL);
+	
+	wxBoxSizer* sizerHor   = new wxBoxSizer(wxHORIZONTAL);
+	
+	wxBoxSizer* sizerAddRem = new wxBoxSizer(wxHORIZONTAL);
+	wxBoxSizer* sizerNavi  = new wxBoxSizer(wxHORIZONTAL);
+
+	wxBoxSizer* sizerActions = new wxBoxSizer(wxHORIZONTAL);
+	
+	wxBoxSizer* sizerLeft  = new wxBoxSizer(wxVERTICAL);	
+	wxBoxSizer* sizerRight = new wxBoxSizer(wxVERTICAL);
+/*...e*/
+
+	sizerHor->Add(sizerLeft, 1, wxEXPAND | wxALL, 5);
+	sizerHor->Add(sizerRight, 1, wxEXPAND | wxALL, 5);
+
+	label = new wxStaticText(this, -1, "Dropdown Element:", wxPoint());
+	sizerLeft->Add(label, 1, wxALL, 5);
+	
+	cBoxFKNames = new wxComboBox(this, -1);
+	sizerLeft->Add(cBoxFKNames, 1, wxALL, 5);
+
+	labelF = new wxStaticText(this, -1, "Feld anzuzeigen:", wxPoint());
+	sizerRight->Add(labelF, 1, wxALL, 5);
+	
+	cBoxPKNames = new wxComboBox(this, -1);
+	sizerRight->Add(cBoxPKNames, 1, wxALL, 5);
+
+	int SelectedColumn = prepareDialogHandler();
+
+	cBoxPKNames->Disable();
+
+	for (int i = 1; i <= query->getColumns(); i++) {
+		char* name = query->getColumnName(i);
+		
+		if (query->hasFKColumn(name) == 1) {
+			cBoxFKNames->Append(wxString(name));
+		}
+	}
+
+	firstButton = new wxButton(this, SelectedColumn, "Ready", wxPoint(), wxSize(100,20));
+	firstButton->Disable();
+
+	sizerNavi->Add(firstButton, 1, wxALL, 5);
+
+	SetAutoLayout(TRUE);
+	
+	sizerMain->Add(sizerHor, 0, wxEXPAND | wxALL, 5);
+	sizerMain->Add(sizerNavi, 0, wxEXPAND | wxALL, 5);
+	
+	SetSizer(sizerMain);
+
+	sizerMain->SetSizeHints(this);
+	sizerMain->Fit(this);
+	
+	Centre();
+
+}
+/*...e*/
+/*...svoid lbConfigure_FK_PK_MappingDialog\58\\58\OnDispatch\40\wxCommandEvent\38\ event \41\:0:*/
+void lbConfigure_FK_PK_MappingDialog::OnDispatch(wxCommandEvent& event ) {
+        switch (event.GetId()) {
+        default:
+                // Delegate all other events
+                {
+                	lbErrCodes err = ERR_NONE;
+			lb_I_Module* m = getModuleInstance();
+			
+			UAP_REQUEST(m, lb_I_EventManager, eman)
+		
+			UAP_REQUEST(m, lb_I_Dispatcher, dispatcher)
+			dispatcher->setEventManager(eman.getPtr());
+
+			UAP_REQUEST(m, lb_I_Integer, param)
+			
+			param->setData(event.GetId());
+			
+			UAP(lb_I_Unknown, uk, __FILE__, __LINE__)
+			QI(param, lb_I_Unknown, uk, __FILE__, __LINE__)
+		
+			UAP_REQUEST(m, lb_I_String, result)
+			UAP(lb_I_Unknown, uk_result, __FILE__, __LINE__)
+			QI(result, lb_I_Unknown, uk_result, __FILE__, __LINE__)
+		
+			dispatcher->dispatch(event.GetId(), uk.getPtr(), &uk_result);
+                }
+                break;
+        }
+}
+/*...e*/
+/*...e*/
+
 /*...sclass lbPluginModuleDatabaseForm implementation:0:*/
 BEGIN_IMPLEMENT_SINGLETON_LB_UNKNOWN(lbPluginModuleDatabaseForm)
         ADD_INTERFACE(lb_I_PluginModule)
@@ -450,6 +824,8 @@ void lbDatabaseDialog::init(char* formName, char* SQLString, char* DBName, char*
 
 	int columns = sampleQuery->getColumns();
 
+	printf("Create %d formular elements.\n", columns);
+
 	sampleQuery->first();
 	
 /*...screate database form elements:8:*/
@@ -461,8 +837,11 @@ void lbDatabaseDialog::init(char* formName, char* SQLString, char* DBName, char*
 		bool createdControl = false;
 
 		UAP(lb_I_Query, FKColumnQuery, __FILE__, __LINE__)
+		UAP(lb_I_Query, FKColumnQuery1, __FILE__, __LINE__)
 		
 		name = strdup(sampleQuery->getColumnName(i));
+
+		printf("Setup form element for '%s'\n", name);
 
 		/* Determine, if the column is a foreign key. If so try to get the
 		   configured column to show instead.
@@ -471,6 +850,8 @@ void lbDatabaseDialog::init(char* formName, char* SQLString, char* DBName, char*
 		if (sampleQuery->hasFKColumn(name) == 1) {
 /*...sCreate a combobox:32:*/
 			lbErrCodes err = ERR_NONE;
+
+printf("Create a drop down box for '%s'\n", name);
 			
 			// Create a mapping instance for this combo box
 			UAP_REQUEST(manager.getPtr(), lb_I_Container, _ComboboxMapper)
@@ -502,6 +883,31 @@ void lbDatabaseDialog::init(char* formName, char* SQLString, char* DBName, char*
 			
 			err = FKColumnQuery->first();
 			
+			if (err == ERR_DB_NODATA) {
+				_CL_LOG << "ERROR: No data column definition to be displayed instead of primary key.\n" LOG_
+			
+	
+				lbConfigure_FK_PK_MappingDialog* fkpkDialog = new lbConfigure_FK_PK_MappingDialog();
+				
+				fkpkDialog->setModuleManager(manager.getPtr(), __FILE__, __LINE__);
+				
+				fkpkDialog->init(sampleQuery.getPtr());
+				
+				fkpkDialog->show();
+				
+				fkpkDialog->destroy();
+
+				FKColumnQuery1 = FKColumnQuery.getPtr();
+			
+				FKColumnQuery = NULL;
+				
+				FKColumnQuery = database->getQuery(0);
+			
+				FKColumnQuery->query(buffer);
+				
+				err = FKColumnQuery->first();
+			}
+
 			if ((err == ERR_NONE) || (err == WARN_DB_NODATA)) {
 /*...sHave mapping to visible data for the combobox:64:*/
 				UAP_REQUEST(manager.getPtr(), lb_I_String, PKName)
@@ -781,9 +1187,11 @@ void lbDatabaseDialog::init(char* formName, char* SQLString, char* DBName, char*
 	
 	Centre();
 
+
 }
 /*...e*/
 
+/*...slbErrCodes LB_STDCALL lbDatabaseDialog\58\\58\lbDBClear\40\\41\:0:*/
 lbErrCodes LB_STDCALL lbDatabaseDialog::lbDBClear() {
 	int columns = sampleQuery->getColumns();
 
@@ -834,7 +1242,10 @@ lbErrCodes LB_STDCALL lbDatabaseDialog::lbDBClear() {
 		
 		free(name);
 	}
+	
+	return ERR_NONE;
 }
+/*...e*/
 
 /*...slbErrCodes LB_STDCALL lbDatabaseDialog\58\\58\lbDBUpdate\40\\41\:0:*/
 lbErrCodes LB_STDCALL lbDatabaseDialog::lbDBUpdate() {
@@ -1306,3 +1717,4 @@ lb_I_Unknown* LB_STDCALL lbPluginDatabaseDialog::getImplementation() {
 /*...e*/
 /*...e*/
 /*...e*/
+
