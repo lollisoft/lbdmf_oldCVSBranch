8dcb4522d8cdd4d1602ccc89ea8a18079e5d1119
lollisoft <lollisoft>
2005-08-15 17:57:28 +0000

    Bugfix in reservation selection. Added actions to print a report.

--- Database/lbDMF-PostgreSQL.sql
+++ Database/lbDMF-PostgreSQL.sql
@@ -131,6 +131,13 @@ insert into action_types (bezeichnung, action_handler, module) values(
 'Open master form',
 'instanceOflbMasterFormAction',
 'lbDatabaseForm');
+
+-- New planned feature. Let create reports based on database queries.
+
+insert into action_types (bezeichnung, action_handler, module) values(
+'Open Database Report',
+'instanceOflbDBReportAction',
+'lbDatabaseReport');
 --...e
 --...sFill actions:0:
 insert into actions (name, typ, source, target) values(
@@ -150,6 +157,28 @@ insert into actions (name, typ, source, target) values(
 1,
 'bezeichnung',
 3);
+
+insert into actions (name, typ, source, target) values(
+'Anwendungen',
+1,
+'userid',
+4);
+
+insert into actions (name, typ, source, target) values(
+'Formulare',
+1,
+'name',
+5);
+
+-- Let print a reservation based on customers id for master report data.
+-- The customer would have more than one reservation. In general, these
+-- reservation entries should be the sub reports.
+
+insert into actions (name, typ, source, target) values(
+'Reservierungen drucken',
+1,
+'kundenid',
+6);
 --...e
 --...sFill action_steps:0:
 insert into action_steps (bezeichnung, a_order_nr, what, type, actionid) values(
@@ -175,12 +204,37 @@ insert into action_steps (bezeichnung, a_order_nr, what, type, actionid) values(
 1,
 'Streckennamen',
 5, 3);
+
+insert into action_steps (bezeichnung, a_order_nr, what, type, actionid) values(
+'Open applications for user',
+1,
+'Benutzer -> Anwendungen',
+4, 4);
+
+insert into action_steps (bezeichnung, a_order_nr, what, type, actionid) values(
+'Open forms for applications',
+1,
+'Formulare',
+4, 5);
+
+insert into action_steps (bezeichnung, a_order_nr, what, type, actionid) values(
+'Print reservations for customers',
+1,
+'DynReservierungen',
+6, 6);
 --...e
 --...sFill formular_actions:0:
 insert into formular_actions (formular, action, event) values(5, 1, 'evt_Reserve_Customer_Trip');
 insert into formular_actions (formular, action, event) values(5, 2, 'evt_Some_Test_Action');
 
 insert into formular_actions (formular, action, event) values(21, 3, 'evt_Manage_Train_Trip_Name');
+
+insert into formular_actions (formular, action, event) values(6, 6, 'evt_Print_Customer_Reservations');
+
+-- Actions for 'lbDMF Manager' application
+
+insert into formular_actions (formular, action, event) values(1, 4, 'evt_Manage_User_Apps');
+insert into formular_actions (formular, action, event) values(2, 5, 'evt_Manage_Apps_Forms');
 --...e
 --...e
 
@@ -740,7 +794,7 @@ insert into Formular_Parameters Values (
 insert into Formular_Parameters Values (
 2,
 'query',
-'select "erwachsene", "kinder", "planfahrtid" from reservierungen',
+'select "erwachsene", "kinder", "planfahrtid", "kundenid" from reservierungen',
 6);
 
 insert into Formular_Parameters Values (
