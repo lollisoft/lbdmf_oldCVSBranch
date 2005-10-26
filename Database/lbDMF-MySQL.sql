345c3f6531fa6a89cd54a34b0ed59300b61d3419
lollisoft <lollisoft>
2005-10-26 05:24:58 +0000

    Adopted to current postgreSQL database model.

--- Database/lbDMF-MySQL.sql
+++ Database/lbDMF-MySQL.sql
@@ -15,9 +15,6 @@ CREATE TABLE column_types
   PRIMARY KEY (id)
 );
 
-insert into column_types (name, tablename, ro) values('kundennr', 'kunden', 'true');
-
-
 -- +---------------------------------------------------------
 -- | TABLE: Actions
 -- | This defines custom actions for an application. These
@@ -46,6 +43,8 @@ CREATE TABLE action_types
 (
   id INTEGER NOT NULL AUTO_INCREMENT,
   bezeichnung char(100),
+  action_handler char(100),	-- the functor with the implementation of the handler
+  module	 char(100),	-- the module with the implementation of the handler
   PRIMARY KEY (id)
 );
 
@@ -56,11 +55,13 @@ CREATE TABLE action_types
 -- | 
 -- +---------------------------------------------------------
 
-CREATE TABLE action_target
+CREATE TABLE action_steps
 (
   id INTEGER NOT NULL AUTO_INCREMENT,
+  actionid	INTEGER,
   bezeichnung	char(100),
   a_order_nr	INTEGER,
+  type		INTEGER, -- May be NULL for the first target
   what		char(100),
   PRIMARY KEY (id)
 );
@@ -70,19 +71,9 @@ CREATE TABLE formular_actions
   id INTEGER NOT NULL AUTO_INCREMENT,
   formular	INTEGER,
   action	INTEGER,
+  event		CHAR(100),
   PRIMARY KEY (id)
 );
-
-
-insert into action_types (bezeichnung) values('Buttonpress');
-
-insert into action_target (bezeichnung, a_order_nr, what) 
-
-	values('reserve_trip', 1, 'evt_Reserve_Customer_Trip');
-
-insert into actions (name, typ, source, target) values('Reserve a trip', 1, 'KundenNr', 1);
-
-insert into formular_actions (formular, action) values(1, 1);
 
 -- +---------------------------------------------------------
 -- | TABLE: Translations
@@ -98,11 +89,9 @@ CREATE TABLE translations
   PRIMARY KEY (id)
 );
 
-
 -- +---------------------------------------------------------
 -- | TABLE: Anwendungen
 -- +---------------------------------------------------------
-
 
 CREATE TABLE Anwendungen
 (
@@ -236,6 +225,7 @@ CREATE TABLE Users
   Vorname VARCHAR(30),
   userid VARCHAR(30),
   passwort VARCHAR(30),
+  lastapp INTEGER,
   PRIMARY KEY (id)
 );
 
@@ -256,59 +246,27 @@ ALTER TABLE `User_Anwendungen` ADD FOREIGN KEY `FK_userid` (`userid`)
 ALTER TABLE `User_Anwendungen` ADD FOREIGN KEY `FK_AnwendungenId` (`AnwendungenId`)
     REFERENCES `Anwendungen` (`id`);
 
+CREATE TABLE report_parameters
+(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  report CHAR(50),
+  name CHAR(50) NOT NULL,
+  value INTEGER  NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE report_texts
+(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  report CHAR(50),
+  line INTEGER,
+  text CHAR(255),
+  PRIMARY KEY (id)
+);
+
+
 -- +---------------------------------------------------------
 -- | DATA
 -- +---------------------------------------------------------
 
-insert into Users Values (1, 'Behrens', 'Lothar', 'behrens', 'password');
-
-insert into Anwendungen Values (1, 'lbDMF Manager', 'Dynamic App Manager', 'lbDynApp', 'instanceOfApplication', 'lb_I_Application');
-insert into Anwendungen Values (2, 'Demo application', 'Demonstration', 'Application', 'instanceOfApplication', 'lb_I_Application');
-insert into Anwendungen Values (3, 'FRS', 'Fahrkarten Reservierungssystem', 'lbDynApp', 'instanceOfApplication', 'lb_I_Application');
-insert into Formulartypen Values (1, '-',                   '-',        'lb_I_DatabaseForm', 'Dynamisch aufgebautes Datenbankformular');
-insert into Formulartypen Values (2, 'lbTrainresBaseForms', 'lbKunden', 'lb_I_FixWiredForm', 'Fest verdrahtetes Formular');
-
-insert into Formulare Values (1, 'Benutzer', 'Benutzer verwalten', 'manageUser', 'Verwaltung der Benutzer in lbDMF',1 , 1);
-insert into Formulare Values (2, 'Formulare', 'Formulare verwalten', 'manageFormulars', 'Verwaltung der Formulare in lbDMF',1 , 1);
-insert into Formulare Values (7, 'Formular_Parameter', 'Formularparameter verwalten', 'manageFormularparameters', 'Verwaltung der Formularparameter in lbDMF',1 , 1);
-insert into Formulare Values (8, 'Benutzer -> Anwendungen', 'Anwendungen Benutzern zuordnen', 'manageAppsUsers', 'Verwaltung der Zuordnung von Benutzern zu Anwendungen', 1, 1);
-insert into Formulare Values (3, 'Kunden', 'Kunden verwalten', 'manageCustomers', 'Bietet Verwaltungsmöglichkeiten für Kunden',2 , 1);
-insert into Formulare Values (4, 'Reservierungen', 'Reservierungen verwalten', 'manageReservations', 'Bietet Verwaltungsmöglichkeiten für Reservierungen von Fahrkarten',2 , 1);
-insert into Formulare Values (5, 'DynKunden', 'Kunden verwalten', 'manageCustomers', 'Bietet Verwaltungsmöglichkeiten für Kunden',3 , 1);
-insert into Formulare Values (6, 'DynReservierungen', 'Reservierungen verwalten', 'manageReservations', 'Bietet Verwaltungsmöglichkeiten für Reservierungen von Fahrkarten',3 , 1);
-
-insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) Values ('Formulare.AnwendungID', 'Formulare', 'Anwendungen.Name', 'Anwendungen');
-insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) Values ('Formulare.Typ', 'Formulare', 'Formulartypen.Beschreibung', 'Formulartypen');
-insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) Values ('FormularID', 'Formular_Parameters', 'Name', 'Formulare');
-insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) Values ('AnwendungID', 'Anwendungs_Parameter', 'Name', 'Anwendungen');
-insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) Values ('userid', 'User_Anwendungen', 'userid', 'Users');
-insert into ForeignKey_VisibleData_Mapping (FKName, FKTable, PKName, PKTable) Values ('AnwendungenId', 'User_Anwendungen', 'Name', 'Anwendungen');
-
-
-insert into Formular_Parameters Values (1, 'query', 'select Anrede, Name, Vorname, Ort, PLZ, Strasse, Vorwahl, Telefon from Kunden', 5);
-insert into Formular_Parameters Values (2, 'query', 'select Name, Vorname, Erwachsene, Kinder from Reservierungen inner join Kunden on Reservierungen.KundenID = Kunden.ID', 6);
-insert into Formular_Parameters Values (3, 'query', 'select Name, Vorname, userid, passwort from Users', 1);
-insert into Formular_Parameters Values (4, 'query', 'select Name, MenuName, EventName, MenuHilfe, AnwendungID, Typ from Formulare', 2);
-insert into Formular_Parameters Values (5, 'query', 'select ParameterName, ParameterValue, FormularID from Formular_Parameters', 7);
-insert into Formular_Parameters Values (6, 'query', 'select AnwendungenId, userid from User_Anwendungen', 8);
-
-insert into Anwendungs_Parameter Values (1, 'DBName', 'trainres', 3);
-insert into Anwendungs_Parameter Values (2, 'DBUser', 'dba', 3);
-insert into Anwendungs_Parameter Values (3, 'DBPass', 'trainres', 3);
-insert into Anwendungs_Parameter Values (4, 'DBName', 'lbDMF', 1);
-insert into Anwendungs_Parameter Values (5, 'DBUser', 'dba', 1);
-insert into Anwendungs_Parameter Values (6, 'DBPass', 'trainres', 1);
-
-insert into Anwendungen_Formulare Values (1, 1, 1);
-insert into Anwendungen_Formulare Values (2, 1, 2);
-insert into Anwendungen_Formulare Values (3, 1, 7);
-insert into Anwendungen_Formulare Values (4, 1, 8);
-insert into Anwendungen_Formulare Values (5, 2, 1);
-insert into Anwendungen_Formulare Values (6, 2, 2);
-insert into Anwendungen_Formulare Values (7, 3, 5);
-insert into Anwendungen_Formulare Values (8, 3, 6);
-
-insert into User_Anwendungen Values (1, 1, 1);
-insert into User_Anwendungen Values (2, 1, 2);
-insert into User_Anwendungen Values (3, 1, 3);
 commit;
