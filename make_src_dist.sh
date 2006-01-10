c6ff9b3bc42b2063a4e00b939450248dfd9e347a
lollisoft <lollisoft>
2006-01-10 23:40:37 +0000

    Attempt for building RPM files

--- make_src_dist.sh
+++ make_src_dist.sh
@@ -12,3 +12,6 @@ cvs -d:ext:lollisoft@cvs.sourceforge.net:/cvsroot/lbdmf export -r HEAD CPP
 cp ../Plugins/DatabaseReport/repwrt.cpp CPP/Plugins/DatabaseReport/repwrt.cpp
 cp ../Plugins/DatabaseReport/repwrt.h CPP/Plugins/DatabaseReport/repwrt.h
 tar cvzf lbDMF-Source-$1.tgz CPP/
+cp lbDMF-Source-$1.tgz /usr/src/packages/SOURCES
+cd ..
+rpmbuild -ba lbDMF.spec
