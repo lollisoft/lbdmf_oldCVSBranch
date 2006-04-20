d0d653498cf62f2e869dfda1afb20466c21ee494
lollisoft <lollisoft>
2006-04-20 12:51:37 +0000

    Working Mac OS X installer creation.

--- make_src_dist.sh
+++ make_src_dist.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
 export DEVROOT=dist/Develop/Projects/CPP
 
+ARCH=`arch`
+
+if [ "$1" == "" ]; then
+	echo No version parameter given.
+	echo Sample: $0 0.7.1
+	exit;
+fi
+
 if [ -e "dist" ]; then
-	rm -R dist;
+	rm -Rf dist;
 fi
 
 mkdir dist
 cd dist
+
+if [ "$ARCH" == "ppc" ]; then
+	mkdir Projects
+	cd Projects
+fi
+
 cvs -d:ext:lollisoft@cvs.sourceforge.net:/cvsroot/lbdmf export -r HEAD CPP
-cp ../Plugins/DatabaseReport/repwrt.cpp CPP/Plugins/DatabaseReport/repwrt.cpp
-cp ../Plugins/DatabaseReport/repwrt.h CPP/Plugins/DatabaseReport/repwrt.h
+cp ~/develop/Projects/CPP/Plugins/DatabaseReport/repwrt.cpp CPP/Plugins/DatabaseReport/repwrt.cpp
+cp ~/develop/Projects/CPP/Plugins/DatabaseReport/repwrt.h CPP/Plugins/DatabaseReport/repwrt.h
 
 # Remove some vendor packages, I do not use yet.
 
 rm -R CPP/vendor/c-goldparser
 
-ARCH=`arch`
-
 if [ "$ARCH" == "ppc" ]; then
+	cp CPP/configure ..
+	/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build \
+		-p ~/Pakete/lbDMF-$1.pkg -f ~/develop/Projects/CPP/dist \
+		-i ~/Pakete/Info.plist -d ~/Pakete/Description.plist
 	exit;
 fi
 
