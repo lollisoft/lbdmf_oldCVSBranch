9bf29a0e0ea7705eea26f7a6927e0af5e7e26aca
lollisoft <lollisoft>
2007-09-16 16:56:02 +0000

    This script should be ok.

--- BaseDevelopment/copy-wxGTK-libs.sh
+++ BaseDevelopment/copy-wxGTK-libs.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-ldd ~/bin/wxWrapper | grep `wx-config --basename` | awk -v VERSION=`wx-config --version-full | awk 'BEGIN { FS="."; } { printf("%s.%s\n", $3, $4); }'` -v PREFIX=$1 'BEGIN { printf("#/bin/sh\n\n"); } { LIBRARY=$1; printf("cp `wx-config --prefix`/lib/%s %s/lib/%s.%s\n", LIBRARY, PREFIX, LIBRARY, VERSION);}' | sh
+ldd $1/bin/wxWrapper | grep `wx-config --basename` | \
+awk -v VERSION=`wx-config --version-full | awk 'BEGIN { FS="."; } { printf("%s.%s\n", $3, $4); }'` \
+    -v PREFIX=$1 '\
+BEGIN {\
+	printf("#/bin/sh\n\n");\
+	printf("cd %s/lib\n", PREFIX);\
+}\
+{\
+	LIBRARY=$1;\
+	printf("cp `wx-config --prefix`/lib/%s %s.%s\n", LIBRARY, LIBRARY, VERSION);\
+        printf("ln -s %s.%s %s\n", LIBRARY, VERSION, LIBRARY);\
+}' | sh
