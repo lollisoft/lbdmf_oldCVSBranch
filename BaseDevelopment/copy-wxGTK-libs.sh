2c6d0330db5370107d9faff10a313381d3d7fee6
lollisoft <lollisoft>
2007-09-16 19:02:48 +0000

    Added wx_base part of libraries.

--- BaseDevelopment/copy-wxGTK-libs.sh
+++ BaseDevelopment/copy-wxGTK-libs.sh
@@ -12,3 +12,16 @@ BEGIN {\
 	printf("cp `wx-config --prefix`/lib/%s %s.%s\n", LIBRARY, LIBRARY, VERSION);\
         printf("ln -s %s.%s %s\n", LIBRARY, VERSION, LIBRARY);\
 }' | sh
+
+ldd $1/bin/wxWrapper | grep wx_base | \
+awk -v VERSION=`wx-config --version-full | awk 'BEGIN { FS="."; } { printf("%s.%s\n", $3, $4); }'` \
+    -v PREFIX=$1 '\
+BEGIN {\
+	printf("#/bin/sh\n\n");\
+	printf("cd %s/lib\n", PREFIX);\
+}\
+{\
+	LIBRARY=$1;\
+	printf("cp `wx-config --prefix`/lib/%s %s.%s\n", LIBRARY, LIBRARY, VERSION);\
+        printf("ln -s %s.%s %s\n", LIBRARY, VERSION, LIBRARY);\
+}' | sh
