48c227d1679119ab906fa2b15e94d12bcfda1f82
lollisoft <lollisoft>
2007-10-28 17:05:46 +0000

    Copying the wxWidgets library to the directory for packaging. The ODBC driver shared objects may also added here.

--- make_src_dist.sh
+++ make_src_dist.sh
@@ -34,6 +34,15 @@ if [ "$ARCH" == "ppc" ]; then
 	/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build \
 		-p ~/Pakete/lbDMF-$1.pkg -f ~/develop/Projects/CPP/dist \
 		-i ~/Pakete/Info.plist -d ~/Pakete/Description.plist
+	rm -r /Users/lothar/develop/Projects/CPP/bindist/Package_contents/usr
+	mkdir /Users/lothar/develop/Projects/CPP/bindist/Package_contents/usr
+	mkdir /Users/lothar/develop/Projects/CPP/bindist/Package_contents/usr/local
+	mkdir /Users/lothar/develop/Projects/CPP/bindist/Package_contents/usr/local/lib
+	cp /usr/local/lib/libwx_macd_Lollisoft-2.6.0.2.0.dylib /Users/lothar/develop/Projects/CPP/bindist/Package_contents/usr/local/lib/libwx_macd_Lollisoft-2.6.0.2.0.dylib
+	cd /Users/lothar/develop/Projects/CPP/bindist/Package_contents/usr/local/lib
+	ln -s libwx_macd_Lollisoft-2.6.0.2.0.dylib libwx_macd_Lollisoft-2.6.0.dylib
+	ln -s libwx_macd_Lollisoft-2.6.0.dylib libwx_macd_Lollisoft-2.6.dylib
+
 	exit;
 fi
 
