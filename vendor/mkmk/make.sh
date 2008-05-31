1fe70e1fb893497eedaa85e077c27e3f8a4ae86c
lollisoft <lollisoft>
2008-05-31 10:02:57 +0000

    Corrected make.sh

--- vendor/mkmk/make.sh
+++ vendor/mkmk/make.sh
@@ -4,12 +4,25 @@ echo Make MKMK for Linux
 
 cd $1
 
-g++-4.1 mkmk.cpp -c -DUNIX \
+if [ $OSTYPE = solaris ]; then
+CPP=g++
+fi
+
+if [ $OSTYPE = linux-debian-ppc ]; then
+CPP=g++-4.1
+fi
+
+if [ $OSTYPE = linux ]; then
+CPP=g++
+fi
+
+
+$CPP mkmk.cpp -c -DUNIX \
     -I /usr/include \
     -I ../dosdir
 
-g++-4.1 contain.cpp -c -DUNIX \
+$CPP contain.cpp -c -DUNIX \
     -I /usr/include \
     -I ../dosdir
 
-g++-4.1 -o mkmk contain.o mkmk.o ../dosdir/match.o ../dosdir/dosdir.o
+$CPP -o mkmk contain.o mkmk.o ../dosdir/match.o ../dosdir/dosdir.o
