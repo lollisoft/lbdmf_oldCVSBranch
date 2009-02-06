5f09c65858db5e531eed2d162d278a8ed3307d5f
lollisoft <lollisoft>
2009-02-06 07:29:00 +0000

    Initial.

---
+++ buildwxWidgets.bat
@@ -0,0 +1,19 @@
+@rem This batch will build the wxWidgets library after the installation.
+
+echo dummy > readme.txt
+
+
+
+echo del readme.txt > doBuildWx.bat
+
+echo set DRIVE=%DEVLW% >> doBuildWx.bat
+echo set WXDIR=%DRIVE%\lbDMF\Develop\wxwin\wx >> doBuildWx.bat
+echo %DRIVE% >> doBuildWx.bat
+echo cd %WXDIR%\build\msw >> doBuildWx.bat
+echo copy /Y %WXDIR%\include\wx\msw\setup.h %WXDIR%\include\wx >> doBuildWx.bat
+echo wmake -f makefile.wat clean >> doBuildWx.bat
+echo wmake -f makefile.wat all >> doBuildWx.bat
+
+
+
+call watcomenv.bat doBuildWx.bat
