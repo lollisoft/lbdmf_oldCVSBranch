2973b75fda22402542c3266e1d01f4bf43298cd4
lollisoft <lollisoft>
2009-07-11 19:49:26 +0000

    Release change to 1.0rc4.

--- interfaces/lbInterfaces.h
+++ interfaces/lbInterfaces.h
@@ -37,7 +37,7 @@
  */
 
 /*...sMain page documentation:0:*/
-/** \mainpage Distributed Multiplatform Framework (1.0rc2)
+/** \mainpage Distributed Multiplatform Framework (1.0rc4)
  * \section intro_sec Introduction to DMF - Distributed Multiplatform Framework
  *
  * This is the introduction for the users of DMF after the first installation.
