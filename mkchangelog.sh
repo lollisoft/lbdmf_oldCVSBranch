3c102381c0c15fc952f5f28a9d5eaa1b1be1e096
lollisoft <lollisoft>
2009-07-28 12:46:53 +0000

    Initial changelog generator.

---
+++ mkchangelog.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Script to generate a changelog file based on the old changelog created prior.
+# The relevant directories are included in the parameters. There may be a changelog entry
+# that was the last entry from the prior changelog. This is due to the missing seconds not reported in the changelog.
+#
+# (c) 2009 Lothar Behrens
+# License; LGPL as the project it self.
+
+
+LASTDATE=`awk -f printRecentChangedate.awk < ChangeLog-basic.txt`
+export LASTDATE
+cvs log -d \>"$LASTDATE" AppDevelopmentDemo/ BaseDevelopment/ Compilers/ Concepts/ Database/ Doc/ include/ interfaces/ make Plugins/ RegressionTests/ Samples/ Test/ vendor/mkmk > ChangeLog
+
+./vendor/cvs2cl/cvs2cl.pl --fsf --header /dev/null -f ChangeLog-basic.txt --stdin < ChangeLog
