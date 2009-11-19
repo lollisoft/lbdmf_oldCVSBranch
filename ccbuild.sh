2d8af5ad8eeb8a89685d87604fbadd14e8511379
lollisoft <lollisoft>
2009-11-19 16:14:15 +0000

    Optimized CC build to write mkmk into /bin.

--- ccbuild.sh
+++ ccbuild.sh
@@ -7,12 +7,18 @@ echo Updating full source tree to be save
 cvs update -d
 
 echo DEVROOT=`pwd` >> makefile
+echo export prefix=`pwd` >> makefile
+echo export PATH=`pwd`/bin:$PATH >> makefile
 
 i=0
 while [ $# -gt 0 ]
 do
   case $1 in
-  "--prefix"*) echo export prefix=`echo $1 | awk -F = '{ print $2; }'` >> makefile;;
+  "--prefix"*) 
+	echo all: BaseDevelopment > makefile
+	echo \# >> makefile
+	echo DEVROOT=`pwd` >> makefile
+	echo export prefix=`echo $1 | awk -F = '{ print $2; }'` >> makefile;;
   esac
   shift
 done
