49e2f3860728f700d73a41f0959a486b75b7a494
lollisoft <lollisoft>
2010-04-24 14:36:55 +0000

    Correction and documented code files.
    More changes to cross check loading DLL's from different compiler.
    It seems that MINGW can't load and use Open Watcom DLL's correctly.
    But it works when I call printf to show a message in that function.
    
    Only returning values as enum types will not work.
    Other code seems to work.
    
    Strange.

--- Test/Console/testmingw/minimaltest/testdll.h
+++ Test/Console/testmingw/minimaltest/testdll.h
@@ -1,3 +1,61 @@
+// Description of test and sample output.
+// 
+// The code demonstrates a problem returning values of type enum.
+// The problem appears when MINGW tries to use a Open Watcom DLL.
+// The problem only appears when the DLL is loaded dynamically.
+// 
+// Q:\develop\Projects\CPP\Test\Console\testmingw\minimaltest>testow
+// Hello from test DLL. Text is 'From text.exe'.
+// Instance of Test created.
+// Hello from test DLL. Text is 'From object out of test.exe'.
+// Hello from test DLL. Text is 'p2'.
+// Have true.
+// Success: ITest->getInt() returns 1
+// Test::release() called.
+// Instance of Test deleted.
+// Test MINGW DLL.
+// Loading testdllmingw DLL dynamically from Open Watcom EXE:
+// Instance of Test created.
+// Success: ITest->getInt() returns 1
+// Test if value is not NULL.
+// Test if value is not NULL.
+// Success: ITest->getInt() returns 1
+// Test OW DLL.
+// Loading testdllow DLL dynamically from Open Watcom EXE:
+// Test instance from OW DLL created via gettestow.
+// Test instance from OW DLL created via _gettestow.
+// OW Instance of Test created.
+// Success: ITest->getInt() returns 1
+// Test if value is not NULL.
+// Test if value is not NULL.
+// Success: ITest->getInt() returns 1
+//
+// Q:\develop\Projects\CPP\Test\Console\testmingw\minimaltest>testmingw
+// Hello from test DLL. Text is 'From text.exe'.
+// Instance of Test created.
+// Hello from test DLL. Text is 'From object out of test.exe'.
+// Hello from test DLL. Text is 'p2'.
+// Have true.
+// Success: ITest->getInt() returns 1
+// Test::release() called.
+// Instance of Test deleted.
+// Test MINGW DLL.
+// Loading testdllmingw DLL dynamically from MINGW32 EXE:
+// Instance of Test created.
+// Success: ITest->getInt() returns 1
+// Test if value is not NULL.
+// Test if value is not NULL.
+// Success: ITest->getInt() returns 1
+// Test OW DLL.
+// Loading testdllow DLL dynamically from MINGW32 EXE:
+// Test instance from OW DLL created via gettestow.
+// Test instance from OW DLL created via _gettestow.
+// OW Instance of Test created.
+// Error: ITest->getInt() doesn't return 1
+// Test if value is not NULL.
+// Test if value is not NULL.
+// Success: ITest->getInt() returns 1
+
 #undef DLLEXPORT
 #ifdef DLL
 #define DLLEXPORT __declspec(dllexport)
