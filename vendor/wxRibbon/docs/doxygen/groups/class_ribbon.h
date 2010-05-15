8bd19abef9a63ac13623a48a5c8a3d440e18143d
lollisoft <lollisoft>
2010-05-15 17:22:08 +0000

    Original version from date as of this checkin.

---
+++ vendor/wxRibbon/docs/doxygen/groups/class_ribbon.h
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        class_ribbon.h
+// Purpose:     Ribbon classes group docs
+// Author:      wxWidgets team
+// RCS-ID:      $Id: class_ribbon.h,v 1.1 2010/05/15 17:22:08 lollisoft Exp $
+// Licence:     wxWindows license
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+
+@defgroup group_class_ribbon Ribbon User Interface
+@ingroup group_class
+
+The wxRibbon library is a set of classes for writing a ribbon user interface.
+At the most generic level, this is a combination of a tab control with a
+toolbar. At a more functional level, it is similar to the user interface
+present in recent versions of Micrsoft Office.
+
+Related Overviews: @ref overview_ribbon
+
+*/
