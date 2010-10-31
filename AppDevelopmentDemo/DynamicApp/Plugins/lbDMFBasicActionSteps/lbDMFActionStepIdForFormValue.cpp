865cbc7e695f0f0a14802927c63217812f498ff4
lollisoft <lollisoft>
2010-10-31 22:13:35 +0000

    Initial.

---
+++ AppDevelopmentDemo/DynamicApp/Plugins/lbDMFBasicActionSteps/lbDMFActionStepIdForFormValue.cpp
@@ -0,0 +1,456 @@
+/*...sLicence:0:*/
+/*
+ DMF Distributed Multiplatform Framework (the initial goal of this library)
+ This file is part of lbDMF.
+ Copyright (C) 2002  Lothar Behrens (lothar.behrens@lollisoft.de)
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+ The author of this work will be reached by e-Mail or paper mail.
+ e-Mail: lothar.behrens@lollisoft.de
+ p-Mail: See my current address on http://www.lollisoft.de/index.php?module=xarpages&func=display&pid=6
+ 
+ */
+/*...e*/
+
+#ifdef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#ifndef UNIX
+#include <windows.h>
+#endif
+#ifdef UNIX
+#endif
+
+#ifndef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+/*...sLB_DMFDATAMODEL_DLL scope:0:*/
+#define LB_DMFDATAMODEL_DLL
+#include <lbdmfdatamodel-module.h>
+/*...e*/
+
+#ifdef __WATCOMC__
+#include <direct.h>
+#endif
+
+#ifdef __MINGW32__
+#include <direct.h>
+#endif
+
+#include <lbDMFIdForFormValue.h>
+
+IMPLEMENT_FUNCTOR(instanceOflbDMFIdForFormValue, lbDMFIdForFormValue)
+
+BEGIN_IMPLEMENT_LB_UNKNOWN(lbDMFIdForFormValue)
+	ADD_INTERFACE(lb_I_DelegatedAction)
+END_IMPLEMENT_LB_UNKNOWN()
+
+
+lbDMFIdForFormValue::lbDMFIdForFormValue() {
+	ref = STARTREF;
+	myActionID = -1;
+
+	_CL_LOG << "lbDMFIdForFormValue::lbDMFIdForFormValue() called." LOG_
+}
+
+lbDMFIdForFormValue::~lbDMFIdForFormValue() {
+	_CL_LOG << "lbDMFIdForFormValue::~lbDMFIdForFormValue() called." LOG_
+}
+
+lbErrCodes LB_STDCALL lbDMFIdForFormValue::setData(lb_I_Unknown*) {
+		_CL_VERBOSE << "Error: lbDMFIdForFormValue::setData(lb_I_Unknown*) not implemented." LOG_
+		return ERR_NOT_IMPLEMENTED;
+}
+
+void LB_STDCALL lbDMFIdForFormValue::setActionID(long id) {
+    myActionID = id;
+}
+
+void LB_STDCALL lbDMFIdForFormValue::setDatabase(lb_I_Database* _db) {
+    db = _db;
+}
+
+void LB_STDCALL lbDMFIdForFormValue::setTransitions(lb_I_Action_Step_Transitions* myTransitions) {
+	
+}
+
+void LB_STDCALL lbDMFIdForFormValue::setParameter(lb_I_ActionStep_Parameters* myParams) {
+	
+}
+
+long LB_STDCALL lbDMFIdForFormValue::execute(lb_I_Parameter* execution_params) {
+	lbErrCodes err = ERR_NONE;
+	// Linear action
+	long next_action = -1;
+
+	const char *p[16 + 1];
+
+	p[0] = NULL;
+	
+	UAP(lb_I_ApplicationParameter, appParams)
+
+	UAP_REQUEST(getModuleInstance(), lb_I_MetaApplication, metaapp)
+	UAP_REQUEST(getModuleInstance(), lb_I_Integer, AppID)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, SourceValue)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, SourceColumn)
+
+	UAP_REQUEST(getModuleInstance(), lb_I_Container, document)
+
+	UAP_REQUEST(getModuleInstance(), lb_I_String, name)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, DBName)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, DBUser)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, DBPass)
+
+	UAP_REQUEST(getModuleInstance(), lb_I_String, currentFormular)
+
+	UAP_REQUEST(getModuleInstance(), lb_I_String, paramName)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, paramValue)
+
+	
+	*name = "source field";
+	execution_params->getUAPString(*&name, *&SourceColumn);
+	
+	*name = "source value";
+	execution_params->getUAPString(*&name, *&SourceValue);
+	
+	*name = "source Form";
+	execution_params->getUAPString(*&name, *&currentFormular);
+	
+	
+	UAP(lb_I_Unknown, ukDoc)
+	UAP(lb_I_Parameter, activeDocument)
+	ukDoc = metaapp->getActiveDocument();
+	if (ukDoc == NULL) {
+		_LOG << "Error: MetaApplication does not have an active document. See log entry, why this happens." LOG_
+		*paramName = "result";
+		*paramValue = "0";
+		
+		execution_params->setUAPString(*&paramName, *&paramValue);
+
+		// The active document contains parameters that are required as input parameters for the action.
+		///\todo Are the parameters always mandatory or can sometimes these parameters provided by user input.
+		return 0;
+	}
+	QI(ukDoc, lb_I_Parameter, activeDocument)
+
+	// Be sure to have the actual data, but save the last changes in settings before.
+    bool b = metaapp->getLoadFromDatabase();
+	metaapp->save();
+	metaapp->setLoadFromDatabase(true);
+	metaapp->load();
+	metaapp->setLoadFromDatabase(b);
+
+	UAP(lb_I_Applications, applications)
+	applications = metaapp->getApplicationModel();
+	applications->selectApplication(ApplicationName->charrep());
+	
+	if (activeDocument != NULL) {
+		UAP(lb_I_KeyBase, key)
+		UAP(lb_I_Unknown, uk)
+		// Get the parameters of the application to be translated.
+		document->setCloning(false);
+		*name = "ApplicationData";
+		activeDocument->getUAPContainer(*&name, *&document);
+		*name = "AppParams";
+		QI(name, lb_I_KeyBase, key)
+		uk = document->getElement(&key);
+		QI(uk, lb_I_ApplicationParameter, appParams)
+		AppID->setData(applications->getApplicationID());
+		
+		// The database I get the current row Id.
+		UAP_REQUEST(getModuleInstance(), lb_I_String, AppDBName)
+		UAP_REQUEST(getModuleInstance(), lb_I_String, AppDBPass)
+		UAP_REQUEST(getModuleInstance(), lb_I_String, AppDBUser)
+		
+		*AppDBName = appParams->getParameter("DBName", (long) AppID->getData());
+		*AppDBUser = appParams->getParameter("DBUser", (long) AppID->getData());
+		*AppDBPass = appParams->getParameter("DBPass", (long) AppID->getData());
+
+		*name = "Formulars";
+		
+		uk = document->getElement(&key);
+		
+		if (uk != NULL) {
+			UAP(lb_I_Formulars, formulars)
+			QI(uk, lb_I_Formulars, formulars)
+			
+			if (formulars != NULL) {
+				formulars->finishFormularIteration();
+				
+				while (formulars->hasMoreFormulars()) {
+					formulars->setNextFormular();
+					
+					if (strcmp(formulars->getName(), currentFormular->charrep()) == 0) {
+						if (formulars->getApplicationID() == (long) AppID->getData()) {
+							long formId = formulars->getFormularID();
+							
+							// Get the query of it
+							*name = "FormParams";
+							uk = document->getElement(&key);
+							
+							if (uk != NULL) {
+								UAP(lb_I_FormularParameter, formParams)
+								QI(uk, lb_I_FormularParameter, formParams)
+								
+								UAP_REQUEST(getModuleInstance(), lb_I_String, query)
+								*query = formParams->getParameter("query", formId);
+								
+								UAP(lb_I_Database, database)
+								
+								if (database == NULL) {
+									char* dbbackend = metaapp->getSystemDatabaseBackend();
+									if (dbbackend != NULL && strcmp(dbbackend, "") != 0) {
+										UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+										AQUIRE_PLUGIN_NAMESPACE_BYSTRING(lb_I_Database, dbbackend, database, "'database plugin'")
+										_LOG << "Using plugin database backend for UML import operation..." LOG_
+									} else {
+										// Use built in
+										REQUEST(getModuleInstance(), lb_I_Database, database)
+										_LOG << "Using built in database backend for UML import operation..." LOG_
+									}
+									
+									if (database == NULL) {
+										_LOG << "Error: Could not load database backend, either plugin or built in version." LOG_
+										chdir(oldcwd);
+										
+										*paramName = "result";
+										*paramValue = "0";
+										
+										execution_params->setUAPString(*&paramName, *&paramValue);
+										
+										return next_action;
+									}
+								}
+								
+								database->init();
+								
+								if (database->connect(AppDBName->charrep(), AppDBName->charrep(), AppDBUser->cherrap(), AppDBPass->charrep()) != ERR_NONE) {
+									_LOG << "Error: Could not connect to given database: '" << AppDBName->charrep() << "'" LOG_
+									
+									*paramName = "result";
+									*paramValue = "0";
+									
+									execution_params->setUAPString(*&paramName, *&paramValue);
+									
+									return next_action;
+								}
+								
+								UAP(lb_I_Query, q)
+								q = database->getQuery(AppDBName->charrep(), 0);
+								
+								if (q->query(query->charrep()) == ERR_NONE) {
+									UAP_REQUEST(getModuleInstance(), lb_I_String, where)
+									bool isChar = q->isCharacterColumn(SourceColumn->charrep());
+									
+									*where = " WHERE ";
+									*where += SourceColumn->charrep(); 
+									*where += " = "
+									if (isChar) *where += "'";
+									*where += SourceValue->charrep(); 
+									if (isChar) *where += "'";
+									
+									*query = q->setWhereClause(query->charrep(), where->charrep());
+									
+									if (q->query(query->charrep()) == ERR_NONE) {
+										// Found some data
+									}
+								}
+								
+								
+							}
+						}
+					}
+				}
+			}
+		}
+			
+			
+	}
+	
+	*paramName = "result";
+	*paramValue = "1";
+	
+	execution_params->setUAPString(*&paramName, *&paramValue);
+	
+	return next_action;
+}
+
+/*...sclass lbPluginDMFIdForFormValue implementation:0:*/
+/*...slbPluginDMFIdForFormValue:0:*/
+class lbPluginDMFIdForFormValue : public lb_I_PluginImpl {
+public:
+	lbPluginDMFIdForFormValue();
+
+	virtual ~lbPluginDMFIdForFormValue();
+
+	bool LB_STDCALL canAutorun();
+	lbErrCodes LB_STDCALL autorun();
+/*...sfrom plugin interface:8:*/
+	void LB_STDCALL initialize();
+
+	bool LB_STDCALL run();
+
+	lb_I_Unknown* LB_STDCALL peekImplementation();
+	lb_I_Unknown* LB_STDCALL getImplementation();
+	void LB_STDCALL releaseImplementation();
+/*...e*/
+
+	DECLARE_LB_UNKNOWN()
+
+	UAP(lb_I_Unknown, ukActions)
+};
+
+BEGIN_IMPLEMENT_LB_UNKNOWN(lbPluginDMFIdForFormValue)
+        ADD_INTERFACE(lb_I_PluginImpl)
+END_IMPLEMENT_LB_UNKNOWN()
+
+IMPLEMENT_FUNCTOR(instanceOflbPluginDMFIdForFormValue, lbPluginDMFIdForFormValue)
+
+/*...slbErrCodes LB_STDCALL lbPluginDMFIdForFormValue\58\\58\setData\40\lb_I_Unknown\42\ uk\41\:0:*/
+lbErrCodes LB_STDCALL lbPluginDMFIdForFormValue::setData(lb_I_Unknown* uk) {
+	lbErrCodes err = ERR_NONE;
+
+	_CL_VERBOSE << "lbPluginDMFIdForFormValue::setData(...) called.\n" LOG_
+
+        return ERR_NOT_IMPLEMENTED;
+}
+/*...e*/
+
+lbPluginDMFIdForFormValue::lbPluginDMFIdForFormValue() {
+	_CL_VERBOSE << "lbPluginDMFIdForFormValue::lbPluginDMFIdForFormValue() called.\n" LOG_
+	ref = STARTREF;
+}
+
+lbPluginDMFIdForFormValue::~lbPluginDMFIdForFormValue() {
+	_CL_VERBOSE << "lbPluginDMFIdForFormValue::~lbPluginDMFIdForFormValue() called.\n" LOG_
+}
+
+bool LB_STDCALL lbPluginDMFIdForFormValue::canAutorun() {
+	return false;
+}
+
+lbErrCodes LB_STDCALL lbPluginDMFIdForFormValue::autorun() {
+	lbErrCodes err = ERR_NONE;
+
+	UAP(lb_I_Database, database)
+	UAP_REQUEST(getModuleInstance(), lb_I_MetaApplication, metaapp)
+
+	if (database == NULL) {
+		char* dbbackend = metaapp->getSystemDatabaseBackend();
+		if (dbbackend != NULL && strcmp(dbbackend, "") != 0) {
+			UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+			AQUIRE_PLUGIN_NAMESPACE_BYSTRING(lb_I_Database, dbbackend, database, "'database plugin'")
+			_LOG << "Using plugin database backend for UML import operation..." LOG_
+		} else {
+			// Use built in
+			REQUEST(getModuleInstance(), lb_I_Database, database)
+			_LOG << "Using built in database backend for UML import operation..." LOG_
+		}
+
+		if (database == NULL) {
+			_LOG << "Error: Could not load database backend, either plugin or built in version." LOG_
+			return ERR_DYNAMIC_APP_LOAD_DBSCHEMA;
+		}
+	}
+
+
+	if (database->init() == ERR_NONE) {
+		if (database->connect("lbDMF", "lbDMF", "dba", "trainres") == ERR_NONE) {
+			UAP(lb_I_Query, q)
+
+			q = database->getQuery("lbDMF", 0);
+
+			if (q != NULL) {
+
+				if (q->query("select id from action_types where action_handler = 'instanceOflbDMFIdForFormValue' and module = 'lbDMFIdForFormValue'") == ERR_NONE) {
+					if (q->first() == ERR_NONE) {
+						// Propably installed
+					} else {
+						// Propably not installed
+					}
+				} else {
+					_LOG << "Error: Could not execute query to check for XSLT action type availability." LOG_
+				}
+			} else {
+				_LOG << "Error: Could not get query object to create XSLT action type." LOG_
+			}
+		} else {
+			_LOG << "Error: Could not log into database to create XSLT action type." LOG_
+		}
+	} else {
+		_LOG << "Error: Could not initialize database to create XSLT action type." LOG_
+	}
+
+	return err;
+}
+
+void LB_STDCALL lbPluginDMFIdForFormValue::initialize() {
+}
+
+bool LB_STDCALL lbPluginDMFIdForFormValue::run() {
+	return true;
+}
+
+/*...slb_I_Unknown\42\ LB_STDCALL lbPluginDMFIdForFormValue\58\\58\peekImplementation\40\\41\:0:*/
+lb_I_Unknown* LB_STDCALL lbPluginDMFIdForFormValue::peekImplementation() {
+	lbErrCodes err = ERR_NONE;
+
+	if (ukActions == NULL) {
+		lbDMFIdForFormValue* DMFXslt = new lbDMFIdForFormValue();
+		DMFXslt->setModuleManager(manager.getPtr(), __FILE__, __LINE__);
+		QI(DMFXslt, lb_I_Unknown, ukActions)
+	} else {
+		_CL_VERBOSE << "lbPluginDatabasePanel::peekImplementation() Implementation already peeked.\n" LOG_
+	}
+
+	return ukActions.getPtr();
+}
+/*...e*/
+/*...slb_I_Unknown\42\ LB_STDCALL lbPluginDMFIdForFormValue\58\\58\getImplementation\40\\41\:0:*/
+lb_I_Unknown* LB_STDCALL lbPluginDMFIdForFormValue::getImplementation() {
+	lbErrCodes err = ERR_NONE;
+
+	if (ukActions == NULL) {
+
+		_CL_VERBOSE << "Warning: peekImplementation() has not been used prior.\n" LOG_
+
+		lbDMFIdForFormValue* xslt = new lbDMFIdForFormValue();
+		xslt->setModuleManager(manager.getPtr(), __FILE__, __LINE__);
+
+		QI(xslt, lb_I_Unknown, ukActions)
+	}
+
+	lb_I_Unknown* r = ukActions.getPtr();
+	ukActions.resetPtr();
+	return r;
+}
+/*...e*/
+void LB_STDCALL lbPluginDMFIdForFormValue::releaseImplementation() {
+        lbErrCodes err = ERR_NONE;
+
+        if (ukActions != NULL) {
+                ukActions--;
+                ukActions.resetPtr();
+        }
+}
+/*...e*/
+/*...e*/
+
---
+++ AppDevelopmentDemo/DynamicApp/Plugins/lbDMFBasicActionSteps/plugin.cpp
@@ -0,0 +1,342 @@
+/*...sLicence:0:*/
+/*
+ DMF Distributed Multiplatform Framework (the initial goal of this library)
+ This file is part of lbDMF.
+ Copyright (C) 2002  Lothar Behrens (lothar.behrens@lollisoft.de)
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+ The author of this work will be reached by e-Mail or paper mail.
+ e-Mail: lothar.behrens@lollisoft.de
+ p-Mail: See my current address on http://www.lollisoft.de/index.php?module=xarpages&func=display&pid=6
+ 
+ */
+/*...e*/
+/*...sincludes:0:*/
+#ifdef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+#ifdef WINDOWS
+#include <windows.h>
+#include <io.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//#include <conio.h>
+
+#ifdef __WATCOMC__
+#include <ctype.h>
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#include <stdio.h>
+#ifndef OSX
+#ifndef DEBUG_MALLOC
+#include <malloc.h>
+#endif
+#endif
+#ifdef OSX
+#include <sys/malloc.h>
+#endif
+
+#ifndef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+
+/*...sLB_PLUGINMANAGER_DLL scope:0:*/
+#define LB_PLUGINMANAGER_DLL
+#include <lbpluginmanager-module.h>
+/*...e*/
+
+/*...e*/
+/*...smisc and includes:0:*/
+#ifdef __GNUG__
+#pragma implementation "plugin.cpp"
+#pragma interface "plugin.cpp"
+#endif
+
+/*...e*/
+
+/*...sclass lbPluginModuleTest:0:*/
+class lbPluginModule : public lb_I_PluginModule {
+public:
+
+	lbPluginModule();
+	virtual ~lbPluginModule();
+
+	DECLARE_LB_UNKNOWN()
+
+	virtual void LB_STDCALL initialize();
+
+	DECLARE_PLUGINS()
+};
+/*...e*/
+
+/*...sclass lbPluginModuleTest implementation:0:*/
+BEGIN_IMPLEMENT_LB_UNKNOWN(lbPluginModule)
+        ADD_INTERFACE(lb_I_PluginModule)
+END_IMPLEMENT_LB_UNKNOWN()
+
+IMPLEMENT_FUNCTOR(instanceOfPluginModule, lbPluginModule)
+
+BEGIN_PLUGINS(lbPluginModule)
+	// Project manager
+	ADD_PLUGIN(lbPluginDMFIdForFormValue, BasicActionSteps)
+END_PLUGINS()
+
+lbPluginModule::lbPluginModule() {
+	ref = STARTREF;
+	_LOG << "lbPluginModule::lbPluginModule() lbPluginDMFIdForFormValue with namespace BasicActionSteps called." LOG_
+}
+
+lbPluginModule::~lbPluginModule() {
+	_LOG << "lbPluginModule::~lbPluginModule() lbPluginDMFIdForFormValue with namespace BasicActionSteps called." LOG_
+}
+
+// The failure code is not late binding the columns, thus a primary key
+// later on seems not to get handled proper when I try to set it readonly.
+//#define FAILURE_CODE
+
+// The query does not cause the stack corrupption either failure or not
+//#define FAILURE_QUERY
+
+void LB_STDCALL lbPluginModule::initialize() {
+	lbErrCodes err = ERR_NONE;
+
+	_LOG << "lbPluginModule::initialize() lbPluginDMFIdForFormValue called." LOG_
+	enumPlugins();
+
+	UAP(lb_I_Database, database)
+	UAP_REQUEST(getModuleInstance(), lb_I_MetaApplication, meta)
+
+	char* dbbackend = meta->getSystemDatabaseBackend();
+	if (dbbackend != NULL && strcmp(dbbackend, "") != 0) {
+		UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+		AQUIRE_PLUGIN_NAMESPACE_BYSTRING(lb_I_Database, dbbackend, database, "'database plugin'")
+		_LOG << "Using plugin database backend for lbPluginModule::initialize() operation..." LOG_
+	} else {
+		// Use built in
+		REQUEST(getModuleInstance(), lb_I_Database, database)
+		
+		if (database == NULL) {
+			_LOG << "Error: Database couldn't loaded. Maybe ODBC components are not installed." LOG_
+			return;
+		}
+		
+		_LOG << "Using built in database backend for lbPluginModule::initialize() operation..." LOG_
+	}
+
+	char* lbDMFPasswd = getenv("lbDMFPasswd");
+	char* lbDMFUser   = getenv("lbDMFUser");
+
+	if (!lbDMFUser) lbDMFUser = "dba";
+	if (!lbDMFPasswd) lbDMFPasswd = "trainres";
+
+	database->init();
+	if (database->connect("lbDMF", "lbDMF", lbDMFUser, lbDMFPasswd) != ERR_NONE) {
+		_LOG << "Error: Could not connect to given database: '" << "lbDMF" << "'" LOG_
+
+		return;
+	}
+
+	UAP(lb_I_Query, q)
+	UAP(lb_I_Query, query_actions)
+	UAP(lb_I_Query, query_action_steps)
+	UAP(lb_I_Query, formularquery)
+	UAP(lb_I_Query, formularactions)
+	UAP(lb_I_Query, applicationparameterquery)
+	UAP(lb_I_Query, applicationquery)
+	UAP_REQUEST(getModuleInstance(), lb_I_String, sql)
+	q = database->getQuery("lbDMF", 0);
+	query_actions = database->getQuery("lbDMF", 0);
+	query_action_steps = database->getQuery("lbDMF", 0);
+	formularquery = database->getQuery("lbDMF", 0);
+	formularactions = database->getQuery("lbDMF", 0);
+	applicationparameterquery = database->getQuery("lbDMF", 0);
+	applicationquery = database->getQuery("lbDMF", 0);
+
+#ifdef FAILURE_QUERY
+	*sql = "select id, bezeichnung, action_handler, module from action_types where module = 'lbDMFXslt' and action_handler = 'instanceOflbDMFXslt'";
+#endif
+#ifndef FAILURE_QUERY
+	*sql = "select id, action_handler, module, bezeichnung from action_types where module = 'lbDMFXslt' and action_handler = 'instanceOflbDMFXslt'";
+#endif
+
+	q->skipFKCollecting();
+	query_actions->skipFKCollecting();
+	query_action_steps->skipFKCollecting();
+	formularquery->skipFKCollecting();
+	formularactions->skipFKCollecting();
+	applicationquery->skipFKCollecting();
+
+#ifdef FAILURE_CODE
+	err = q->query(sql->charrep());
+#endif
+#ifndef FAILURE_CODE
+	err = q->query(sql->charrep(), false);
+	q->setReadonly("id");
+    q->bind();
+#endif
+
+	if (err == ERR_NONE) {
+		err = q->first();
+		if ((err == ERR_NONE) || (err == WARN_DB_NODATA)) {
+			_LOG << "Plugin lbPluginDMFIdForFormValue with BasicActionSteps already installed." LOG_
+		} else {
+			if (meta->askYesNo("Plugin for XSLT transformation actions available.\n\nWould you enable that plugin ?")) {
+				UAP_REQUEST(getModuleInstance(), lb_I_String, columnname)
+				UAP_REQUEST(getModuleInstance(), lb_I_String, module)
+				UAP_REQUEST(getModuleInstance(), lb_I_String, action_handler)
+				UAP_REQUEST(getModuleInstance(), lb_I_String, bezeichnung)
+				UAP_REQUEST(getModuleInstance(), lb_I_String, id)
+				UAP_REQUEST(getModuleInstance(), lb_I_Long, action_type_id)
+
+				// If loading is set to file, overwrite.
+				meta->setLoadFromDatabase(true);
+
+				*module = "lbDMFXslt";
+				*action_handler = "instanceOflbDMFXslt";
+				*bezeichnung = "Perform XSLT transformation";
+				*id = "1";
+
+				*columnname = "module";
+				q->setString(*&columnname, *&module);
+				*columnname = "id";
+				q->setString(*&columnname, *&id);
+				*columnname = "action_handler";
+				q->setString(*&columnname, *&action_handler);
+				*columnname = "bezeichnung";
+				q->setString(*&columnname, *&bezeichnung);
+
+				q->add();
+				*columnname = "id";
+				q->setString(*&columnname, *&id);
+				*columnname = "module";
+				q->setString(*&columnname, *&module);
+				*columnname = "action_handler";
+				q->setString(*&columnname, *&action_handler);
+				*columnname = "bezeichnung";
+				q->setString(*&columnname, *&bezeichnung);
+
+#ifdef FAILURE_CODE
+                q->setReadonly("id");
+#endif
+				q->update();
+
+				q->last();
+				action_type_id = q->getAsLong(1);
+
+				// Action to generate code at the level of Application
+				{
+					query_actions->query("insert into actions (name, typ, source, target) values('Code generieren', 1, 'name', 0)");
+                    _LOG << "Initialize lbDMFXSLT module. Step 4" LOG_
+
+					UAP_REQUEST(getModuleInstance(), lb_I_String, sql)
+					UAP(lb_I_Long, actionid)
+
+					if (query_actions->query("select id from actions where name = 'Code generieren'") == ERR_NONE) {
+						err = query_actions->first();
+						if ((err == ERR_NONE) || (err == WARN_DB_NODATA)) {
+							actionid = query_actions->getAsLong(1);
+
+							*sql = "insert into action_steps (actionid, type, bezeichnung, a_order_nr) values (";
+							*sql += actionid->charrep();
+							*sql += ", ";
+							*sql += action_type_id->charrep();
+							*sql += ", 'Translate XML file with XSLT template', 1)";
+							query_action_steps->query(sql->charrep());
+
+							*sql = "select id from formulare where name = 'Anwendungen' and anwendungid in (select id from anwendungen where name = 'lbDMF Manager')";
+
+							if (formularquery->query(sql->charrep()) == ERR_NONE) {
+								err = formularquery->first();
+								if ((err == ERR_NONE) || (err == WARN_DB_NODATA)) {
+									UAP(lb_I_String, formid)
+
+									formid = formularquery->getAsString(1);
+
+									*sql = "insert into formular_actions (formular, action, event) values (";
+									*sql += formid->charrep();
+									*sql += ", ";
+									*sql += actionid->charrep();
+									*sql += ", 'evtGenerateCode')";
+
+									formularactions->query(sql->charrep());
+								}
+							}
+						}
+					}
+				}
+				// Action to generate XSLT template at the level of Templates
+#ifdef bla
+				{
+					query_actions->query("insert into actions (name, typ, source, target) values('generate XSLT template', 1, 'name', 0)");
+
+					UAP_REQUEST(getModuleInstance(), lb_I_String, sql)
+					UAP(lb_I_Long, actionid)
+
+					if (query_actions->query("select id from actions where name = 'generate XSLT template'") == ERR_NONE) {
+						err = query_actions->first();
+						if ((err == ERR_NONE) || (err == WARN_DB_NODATA)) {
+							actionid = query_actions->getAsLong(1);
+
+							*sql = "insert into action_steps (actionid, type, bezeichnung, a_order_nr) values (";
+							*sql += actionid->charrep();
+							*sql += ", ";
+							*sql += action_type_id->charrep();
+							*sql += ", 'Create  XSLT template file set based on template definition', 1)";
+							query_action_steps->query(sql->charrep());
+
+							*sql = "select id from formulare where name = 'Templates' and anwendungid in (select id from anwendungen where name = 'lbDMF Manager')";
+
+							if (formularquery->query(sql->charrep()) == ERR_NONE) {
+								err = formularquery->first();
+								if ((err == ERR_NONE) || (err == WARN_DB_NODATA)) {
+									UAP(lb_I_String, formid)
+
+									formid = formularquery->getAsString(1);
+
+									*sql = "insert into formular_actions (formular, action, event) values (";
+									*sql += formid->charrep();
+									*sql += ", ";
+									*sql += actionid->charrep();
+									*sql += ", 'evtGenerateXSLTTemplate')";
+
+									formularactions->query(sql->charrep());
+								}
+							}
+						}
+					}
+				}
+#endif
+			}
+		}
+	}
+}
+
+lbErrCodes LB_STDCALL lbPluginModule::setData(lb_I_Unknown* uk) {
+        _CL_VERBOSE << "lbPluginModuleTest::setData(...) not implemented yet" LOG_
+
+        return ERR_NOT_IMPLEMENTED;
+}
+/*...e*/
