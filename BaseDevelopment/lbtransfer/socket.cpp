9ec5694b6b194d74b96d8933e23e965eda3ada59
lollisoft <lollisoft>
2011-01-23 09:36:57 +0000

    Removed the slow waits.

--- BaseDevelopment/lbtransfer/socket.cpp
+++ BaseDevelopment/lbtransfer/socket.cpp
@@ -871,9 +871,10 @@ lbErrCodes lbSocket::recv(void* buf, short & len) {
     int lastError = 0;
 	short nlen;
 
-  while (isValid() == 0) {
+/*  while (isValid() == 0) {
   	lb_sleep(100);
-  } 
+  }
+ */
 /*...sWINDOWS:0:*/
 #ifdef WINDOWS
 
@@ -1095,10 +1096,10 @@ lbErrCodes lbSocket::recv_charbuf(char *buf)
     }
 
     // Wait a bit.
-	while (isValid() == 0) {
+/*	while (isValid() == 0) {
 		lb_sleep(100);
 	} 
-
+*/
 /*...sWINDOWS:0:*/
 #ifdef WINDOWS
 /*...sserver:0:*/
