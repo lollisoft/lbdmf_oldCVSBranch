6efd99b7610bd8ad9c0d32c4a339e5a68dd8663e
lollisoft <lollisoft>
2011-06-12 05:53:48 +0000

    Changed version number and refactored their usage.

--- Test/GUI/wxWrapper/postlink-mac.sh
+++ Test/GUI/wxWrapper/postlink-mac.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Copies together files for the Mac OS X application bundle and created a disk image
 
+export VERSION=1.0.2
+
 cp ../../../Database/*.sql wxWrapper.app/Contents/Resources
 cp splash.png wxWrapper.app/Contents/Resources
 mkdir wxWrapper.app/Contents/Resources/toolbarimages
@@ -15,36 +17,36 @@ cp -R `wx-config --prefix`/lib/lib`wx-config --basename`-`wx-config --release`.d
 
 # Creating a new diskimage
 
-hdiutil create -ov -size 80m -volname lbDMF-1.0.1 lbDMF-1.0.1-`uname -p`.dmg -fs HFS+
+hdiutil create -ov -size 80m -volname lbDMF-$VERSION lbDMF-$VERSION-`uname -p`.dmg -fs HFS+
 
 sleep 5
 
-hdiutil attach lbDMF-1.0.1-`uname -p`.dmg
+hdiutil attach lbDMF-$VERSION-`uname -p`.dmg
 
 # Copy stuff
 
-#mkdir /Volumes/lbDMF-1.0.1/`uname -p`
+#mkdir /Volumes/lbDMF-$VERSION/`uname -p`
 
-#cp -R wxWrapper.app /Volumes/lbDMF-1.0.1/`uname -p`
-cp -R wxWrapper.app /Volumes/lbDMF-1.0.1
-mkdir /Volumes/lbDMF-1.0.1/toolbarimages
-cp toolbarimages/*.xpm /Volumes/lbDMF-1.0.1/toolbarimages
-cp toolbarimages/*.png /Volumes/lbDMF-1.0.1/toolbarimages
-cp -R ../../../AppDevelopmentDemo/DynamicApp/UMLSamples /Volumes/lbDMF-1.0.1
-cp ../../../COPYING /Volumes/lbDMF-1.0.1
-cp ../../../license-bindist.txt /Volumes/lbDMF-1.0.1
-mkdir /Volumes/lbDMF-1.0.1/XSLT
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/include            /Volumes/lbDMF-1.0.1/XSLT/include
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/DMFToXMI           /Volumes/lbDMF-1.0.1/XSLT/DMFToXMI
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/XMIToDMF           /Volumes/lbDMF-1.0.1/XSLT/XMIToDMF
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/lbDMFDataViewModel /Volumes/lbDMF-1.0.1/XSLT/lbDMFDataViewModel
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/lbDMFFixedFormular /Volumes/lbDMF-1.0.1/XSLT/lbDMFFixedFormular
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/TurboVision        /Volumes/lbDMF-1.0.1/XSLT/TurboVision
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/wxActiveRecords    /Volumes/lbDMF-1.0.1/XSLT/wxActiveRecords
-cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/wxLua              /Volumes/lbDMF-1.0.1/XSLT/wxLua
-cp ../../../AppDevelopmentDemo/DynamicApp/Doc/ApplicationprototypingDokumentation.pdf /Volumes/lbDMF-1.0.1/
+#cp -R wxWrapper.app /Volumes/lbDMF-$VERSION/`uname -p`
+cp -R wxWrapper.app /Volumes/lbDMF-$VERSION
+mkdir /Volumes/lbDMF-$VERSION/toolbarimages
+cp toolbarimages/*.xpm /Volumes/lbDMF-$VERSION/toolbarimages
+cp toolbarimages/*.png /Volumes/lbDMF-$VERSION/toolbarimages
+cp -R ../../../AppDevelopmentDemo/DynamicApp/UMLSamples /Volumes/lbDMF-$VERSION
+cp ../../../COPYING /Volumes/lbDMF-$VERSION
+cp ../../../license-bindist.txt /Volumes/lbDMF-$VERSION
+mkdir /Volumes/lbDMF-$VERSION/XSLT
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/include            /Volumes/lbDMF-$VERSION/XSLT/include
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/DMFToXMI           /Volumes/lbDMF-$VERSION/XSLT/DMFToXMI
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/XMIToDMF           /Volumes/lbDMF-$VERSION/XSLT/XMIToDMF
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/lbDMFDataViewModel /Volumes/lbDMF-$VERSION/XSLT/lbDMFDataViewModel
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/lbDMFFixedFormular /Volumes/lbDMF-$VERSION/XSLT/lbDMFFixedFormular
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/TurboVision        /Volumes/lbDMF-$VERSION/XSLT/TurboVision
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/wxActiveRecords    /Volumes/lbDMF-$VERSION/XSLT/wxActiveRecords
+cp -R ../../../AppDevelopmentDemo/DynamicApp/XSLT_Templates/wxLua              /Volumes/lbDMF-$VERSION/XSLT/wxLua
+cp ../../../AppDevelopmentDemo/DynamicApp/Doc/ApplicationprototypingDokumentation.pdf /Volumes/lbDMF-$VERSION/
 
-cat <<EOF >> /Volumes/lbDMF-1.0.1/Readme.txt
+cat <<EOF >> /Volumes/lbDMF-$VERSION/Readme.txt
 Dear Mac user!
 
 If you start designing your database applications on one platform (either Intel or PPC) and switch over to a later time, please copy the *.db3 *.mad and *.daf files in the application bundle to the other bundle at the same place (in Resources).
@@ -58,6 +60,6 @@ Thanks
 Lothar Behrens
 EOF
 
-rm -rf `find /Volumes/lbDMF-1.0.1 -name CVS -print`
+rm -rf `find /Volumes/lbDMF-$VERSION -name CVS -print`
 
-hdiutil detach /Volumes/lbDMF-1.0.1
+hdiutil detach /Volumes/lbDMF-$VERSION
