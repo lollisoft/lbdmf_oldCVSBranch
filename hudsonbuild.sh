2b770fc454bbee94b6fcd99ff5b92792e94a3bc5
lollisoft <lollisoft>
2011-10-30 10:10:24 +0000

    Changed the prefix a bit.

--- hudsonbuild.sh
+++ hudsonbuild.sh
@@ -38,7 +38,7 @@ make
 cd BaseDevelopment
 CRUISECONTROL=
 export CRUISECONTROL
-make -e prefix=$(prefix) install
+make -e prefix=`pwd`/.. install
 cd ..
 LD_LIBRARY_PATH=$(pwd)/lib
 export LD_LIBRARY_PATH
