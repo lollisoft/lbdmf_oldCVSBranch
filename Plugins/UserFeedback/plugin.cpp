83a5cb8e010f46612789e7fc7df4f92de93b409c
lollisoft <lollisoft>
2012-04-21 10:30:53 +0000

    A new user feedback plugin.

---
+++ Plugins/UserFeedback/plugin.cpp
@@ -0,0 +1,126 @@
+/*...sLicence:0:*/
+/*
+ DMF Distributed Multiplatform Framework (the initial goal of this library)
+ This file is part of lbDMF.
+ Copyright (C) 2002  Lothar Behrens (lothar.behrens@lollisoft.de)
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ 
+ The author of this work will be reached by e-Mail or paper mail.
+ e-Mail: lothar.behrens@lollisoft.de
+ p-Mail: See my current address on http://www.lollisoft.de/index.php?module=xarpages&func=display&pid=6
+ 
+ */
+/*...e*/
+/*...sincludes:0:*/
+#ifdef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+#ifdef WINDOWS
+#include <windows.h>
+#include <io.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+	//#include <conio.h>
+	
+#ifdef __WATCOMC__
+#include <ctype.h>
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#include <stdio.h>
+#ifndef OSX
+#include <malloc.h>
+#endif
+#ifdef OSX
+#include <sys/malloc.h>
+#endif
+
+#ifndef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+
+/*...sLB_PLUGINMANAGER_DLL scope:0:*/
+#define LB_PLUGINMANAGER_DLL
+#include <lbpluginmanager-module.h>
+/*...e*/
+
+/*...e*/
+
+/*...sclass lbPluginModuleUserFeedback:0:*/
+class lbPluginModuleUserFeedback : public lb_I_PluginModule {
+public:
+	
+	lbPluginModuleUserFeedback();
+	virtual ~lbPluginModuleUserFeedback();
+	
+	DECLARE_LB_UNKNOWN()
+	
+	void LB_STDCALL initialize();
+	void LB_STDCALL install();
+	
+	DECLARE_PLUGINS()
+};
+/*...e*/
+
+/*...sclass lbPluginModuleVisitors implementation:0:*/
+BEGIN_IMPLEMENT_LB_UNKNOWN(lbPluginModuleUserFeedback)
+ADD_INTERFACE(lb_I_PluginModule)
+END_IMPLEMENT_LB_UNKNOWN()
+
+IMPLEMENT_FUNCTOR(instanceOfPluginModule, lbPluginModuleUserFeedback)
+
+BEGIN_PLUGINS(lbPluginModuleUserFeedback)
+ADD_PLUGIN(lbPluginUserFeedback,			UserFeedback)
+END_PLUGINS()
+
+
+
+lbPluginModuleUserFeedback::lbPluginModuleUserFeedback() {
+	
+}
+
+lbPluginModuleUserFeedback::~lbPluginModuleUserFeedback() {
+	_CL_VERBOSE << "lbPluginModuleUserFeedback::~lbPluginModuleUserFeedback() called." LOG_
+}
+
+void LB_STDCALL lbPluginModuleUserFeedback::initialize() {
+	_CL_VERBOSE << "lbPluginModuleUserFeedback::initialize() called." LOG_
+	enumPlugins();
+}
+
+void LB_STDCALL lbPluginModuleUserFeedback::install() {
+
+}
+
+lbErrCodes LB_STDCALL lbPluginModuleUserFeedback::setData(lb_I_Unknown* uk) {
+	_CL_VERBOSE << "lbPluginModuleUserFeedback::setData(...) not implemented yet" LOG_
+	
+	if (uk != NULL) {
+		_CL_LOG << "Cloning object with " << uk->getRefCount() << " references." LOG_
+	}
+	
+	return ERR_NOT_IMPLEMENTED;
+}
+/*...e*/
