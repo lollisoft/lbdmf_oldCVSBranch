fc1c0a6b81b470c30b508dc9aaf575eea36295e5
lollisoft <lollisoft>
2012-06-10 06:10:23 +0000

    Do not clean a previous build. Deactivated path to bakefile.

--- buildwxWidgets_MinGW.bat
+++ buildwxWidgets_MinGW.bat
@@ -10,7 +10,7 @@ set Path=%SystemRoot%\system32
 
 set Path=%Path%;%MINGWBIN%
 
-set Path=%Path%;Q:\develop\Tools\Bakefile\src
+rem set Path=%Path%;Q:\develop\Tools\Bakefile\src
 
 echo del readme.txt > doBuildWx.bat
 
@@ -19,7 +19,7 @@ echo set WXDIR=%DRIVE%\lbDMF\Develop\wxwin\wx >> doBuildWx.bat
 echo %DRIVE% >> doBuildWx.bat
 echo cd %WXDIR%\build\msw >> doBuildWx.bat
 echo copy /Y %WXDIR%\include\wx\msw\setup.h %WXDIR%\include\wx >> doBuildWx.bat
-echo mingw32-make -f makefile.gcc clean >> doBuildWx.bat
+rem echo mingw32-make -f makefile.gcc clean >> doBuildWx.bat
 echo mingw32-make -f makefile.gcc all >> doBuildWx.bat
 
 
