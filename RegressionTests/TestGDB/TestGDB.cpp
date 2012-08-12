aeea02c546c289421239b311d5719c81bbec3382
lollisoft <lollisoft>
2012-08-12 07:37:48 +0000

    In the previous version a break point could be set at DoTest(), but not at main. In the current version breaking both, DoTest() and Dotest1() points to the same line of code. Why is that happen?

--- RegressionTests/TestGDB/TestGDB.cpp
+++ RegressionTests/TestGDB/TestGDB.cpp
@@ -73,12 +73,21 @@ void DoTest()
 	meta->msgBox("Test", "Message");	
 }
 
+void DoTest1()
+{
+	UAP_REQUEST(getModuleInstance(), lb_I_String, s)
+	UAP_REQUEST(getModuleInstance(), lb_I_MetaApplication, meta)
+		
+	meta->msgBox("Test", "Message");	
+}
+
 // http://www.google.co.in/#hl=de&sclient=psy-ab&q=gcc+extract+dwarf+info&oq=gcc+extract+dwarf+info&gs_l=serp.3...4852.16604.0.16862.24.23.1.0.0.0.213.2453.15j7j1.23.0...0.0...1c.V9vmdrT3ctk&pbx=1&bav=on.2,or.r_gc.r_pw.r_qf.&fp=1391debd1f706dcc&biw=1920&bih=953
 // http://eli.thegreenplace.net/2011/02/07/how-debuggers-work-part-3-debugging-information/
 int main(int argc, char *argv[]) {
 	lbErrCodes err = ERR_NONE;
 	
 	DoTest();
+	DoTest1();
 	
 	unHookAll();
 	
