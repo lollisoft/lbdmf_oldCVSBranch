acc4ed46bf7262bdfb8f46956b0df8e2675fe5d2
lollisoft <lollisoft>
2012-12-21 06:28:20 +0000

    Removing wxWidgets related pligins in unit test as it makes no sense and if a console application will be deployed, these wouldn't include wxWidgets related stuff.

--- hudsonbuild_unit_test.sh
+++ hudsonbuild_unit_test.sh
@@ -7,4 +7,17 @@ LD_LIBRARY_PATH=$(pwd)/lib
 export LD_LIBRARY_PATH
 PLUGIN_DIR=$(pwd)/plugins
 export PLUGIN_DIR
+
+# Remove all wxWidgets based plugins that are not installed in a console based application.
+# wxWidgets based stuff does not make sense here as I did not use wsWidgets outside of GUI.
+
+rm $(pwd)/plugins/lbCryptoStream.*
+rm $(pwd)/plugins/lbDatabaseForm.*
+rm $(pwd)/plugins/lbDatabaseReport.*
+rm $(pwd)/plugins/lbDMFProject.*
+rm $(pwd)/plugins/lbLoginWizard.*
+rm $(pwd)/plugins/lbwxSFDesigner.*
+rm $(pwd)/plugins/UserFeedback.*
+
+
 $(pwd)/bin/lbDMFUnitTests
