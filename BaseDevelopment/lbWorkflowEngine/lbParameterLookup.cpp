2a794acd4d479ffb22e465195ffb9ce852190e57
lollisoft <lollisoft>
2013-02-23 08:20:18 +0000

    Refactored.

---
+++ BaseDevelopment/lbWorkflowEngine/lbParameterLookup.cpp
@@ -0,0 +1,92 @@
+/*...sLicence:0:*/
+/*
+    DMF Distributed Multiplatform Framework (the initial goal of this library)
+    This file is part of lbDMF.
+    Copyright (C) 2002  Lothar Behrens (lothar.behrens@lollisoft.de)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+    The author of this work will be reached by e-Mail or paper mail.
+    e-Mail: lothar.behrens@lollisoft.de
+    p-Mail: Lothar Behrens
+            Ginsterweg 4
+
+            65760 Eschborn (germany)
+*/
+/*...e*/
+
+/*...sincludes:0:*/
+#include <lbDMF_wxPrec.h>
+
+#ifdef WINDOWS
+#include <windows.h>
+#include <io.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//#include <conio.h>
+
+#ifdef __WATCOMC__
+#include <ctype.h>
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#include <stdio.h>
+#ifndef OSX
+#ifndef USE_MPATROL
+#include <malloc.h>
+#endif
+#endif
+#ifdef OSX
+#include <sys/malloc.h>
+#endif
+
+#ifndef LBDMF_PREC
+#include <lbConfigHook.h>
+#endif
+
+
+/*...sLB_PLUGINMANAGER_DLL scope:0:*/
+#define LB_PLUGINMANAGER_DLL
+#include <lbpluginmanager-module.h>
+/*...e*/
+
+/*...e*/
+/*...smisc and includes:0:*/
+#ifdef __GNUG__
+#pragma implementation "dynamic.cpp"
+#pragma interface "dynamic.cpp"
+#endif
+
+#include <lbParameterLookup.h>
+
+//		char* LB_STDCALL lookupParameter(lb_I_FormularParameter* from, const char* name, long ApplicationID);
+char* LB_STDCALL lookupParameter(lb_I_FormularParameter* from, const char* name, long FormID) {
+	from->finishIteration();
+	
+	while (from->hasMoreElements()) {
+		from->setNextElement();
+		if (from->get_formularid() == FormID && strcmp(from->get_parametername(), name) == 0)
+			return from->get_parametervalue();
+	}
+	_LOG << "getParameter(...) Error: Parameter not found." LOG_
+	return NULL;
+}
---
+++ BaseDevelopment/lbWorkflowEngine/lbParameterLookup.h
@@ -0,0 +1,3 @@
+
+
+char* LB_STDCALL lookupParameter(lb_I_FormularParameter* from, const char* name, long FormID);
