7be075a9d68e7538aff0791c22568ad638a0942d
lollisoft <lollisoft>
2013-11-17 11:34:58 +0000

    Changed version

--- Test/GUI/wxWrapper/postlink-mac.sh
+++ Test/GUI/wxWrapper/postlink-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Copies together files for the Mac OS X application bundle and created a disk image
 
-export VERSION=1.0.4_final
+export VERSION=1.0.4.1_final
 
 cp ../../../Database/*.sql wxWrapper.app/Contents/Resources
 cp splash.png wxWrapper.app/Contents/Resources
