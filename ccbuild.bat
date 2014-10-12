623f0abe27624cf1e85ac61a2fcb98d0730f8524
lollisoft <lollisoft>
2014-10-12 09:17:12 +0000

    Added path to call.

--- ccbuild.bat
+++ ccbuild.bat
@@ -29,7 +29,7 @@ echo * Using a properly set up ODBC database (tested is PostgreSQL), no problems
 echo * known.                                                                        * >> readme.txt
 echo --------------------------------------------------------------------------------- >> readme.txt 
 
-call watcomenv.bat none CC
+call Projects\CPP\watcomenv.bat none CC
 
 cd %DEVROOT%
 
