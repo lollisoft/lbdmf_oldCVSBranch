b9a24885c7156270aaf50d9f1c90b14e0f10b1b5
lollisoft <lollisoft>
2014-11-22 14:28:16 +0000

    Added installation steps for MinGW if required.

--- ccbuild.bat
+++ ccbuild.bat
@@ -7,6 +7,14 @@ echo ---------------------------------------------------------------------------
 
 call Projects\CPP\watcomenv.bat none JENKINS
 
+@rem Get an explicite version that always ensures to build the code
+set MINGW_STICKON_VERSION=4.7.*
+set MINGW_STICKON_WIN32_VERSION=4.0.3-*
+
+IF NOT EXIST %DEVLW%\lbDMF\Tools\MinGW\bin\gcc.exe (
+call Projects\CPP\installMinGW.bat %MINGW_STICKON_VERSION% %MINGW_STICKON_WIN32_VERSION% 
+)
+
 if "%1" == "release" (
 
 cd ..
