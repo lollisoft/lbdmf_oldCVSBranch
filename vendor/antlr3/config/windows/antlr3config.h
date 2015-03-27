7b25a9e405673e77a554407cec139b93aed92ee6
lollisoft <lollisoft>
2015-03-27 06:32:42 +0000

    Switched socket include.

--- vendor/antlr3/config/windows/antlr3config.h
+++ vendor/antlr3/config/windows/antlr3config.h
@@ -50,7 +50,7 @@
 #define HAVE_RESOLV_H 1
 
 /* Define to 1 if you have the <socket.h> header file. */
-/* #undef HAVE_SOCKET_H */
+#define HAVE_SOCKET_H 1
 
 /* Define to 1 if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
@@ -74,7 +74,7 @@
 #define HAVE_SYS_MALLOC_H 1
 
 /* Define to 1 if you have the <sys/socket.h> header file. */
-#define HAVE_SYS_SOCKET_H 1
+/* #undef HAVE_SYS_SOCKET_H 1 */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
