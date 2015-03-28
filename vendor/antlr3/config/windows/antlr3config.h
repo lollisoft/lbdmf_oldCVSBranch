64e5fbfd1a44a872da09ef52bee4381ee54325d0
lollisoft <lollisoft>
2015-03-28 06:18:28 +0000

    Deactivated debugger socket feature by correcting define.

--- vendor/antlr3/config/windows/antlr3config.h
+++ vendor/antlr3/config/windows/antlr3config.h
@@ -2,7 +2,7 @@
 /* antlr3config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define if ANTLR debugger not required */
-#define ANTLR3_NODEBUGGER
+#define ANTLR3_NODEBUGGER 1
 
 /* Define if 64 bit mode required */
 /* #undef ANTLR3_USE_64BIT */
