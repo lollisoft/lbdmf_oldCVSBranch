9c263ac8717991cdcdc947d1a04de24a9ec4defb
lothar <lothar>
2023-04-16 14:47:23 +0000

    Used the wrong parameter

--- watcomenv.bat
+++ watcomenv.bat
@@ -288,7 +288,7 @@ IF NOT EXIST %DEVROOT%\wxwin (
 cd %DEVLW%\q 
 cd %DEVROOT%
 set PATH=%PATH%;C:\Q\develop\Tools\7Zip\7-Zip
-7z e %DEVROOT%\Projects\CPP\vendor\wxMSW-2.8.12.zip -owxwin -aoa
+7z x %DEVROOT%\Projects\CPP\vendor\wxMSW-2.8.12.zip -owxwin -aoa
 rem call %DEVROOT%\Projects\CPP\buildwxWidgets_MinGW_CC.bat
 cd %DEVROOT%\Projects\CPP\BaseDevelopment
 )
