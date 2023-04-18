6fa05702b344032fa7c91614f8ba85cf90c65f08
anonymous <anonymous>
2023-04-18 13:30:46 +0000

    Added PPC target and structured it a bit

--- collect_releases.sh
+++ collect_releases.sh
@@ -8,3 +8,10 @@ wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_
 wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Source-1.3.2.exe
 wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-XSLTPatchDistribution-1.0.4-final-patch_1.exe
 
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SRPMS/lbdmf-1.3.2-1.src.rpm
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/RPMS/i586/lbdmf-1.3.2-1.i586.rpm
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SOURCES/lbdmf-1.3.2.tgz
+
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_OSX_Release/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.2-i386.dmg.zip
+
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_PPC/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.2-powerpc.dmg.zip
