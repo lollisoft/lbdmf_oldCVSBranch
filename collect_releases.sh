709cf393a73a2d82509b8b3eb35e56f55685a849
lothar <lothar>
2023-04-18 12:52:06 +0000

    Initial release collector

---
+++ collect_releases.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinbuildTools-1.0.4.4-final.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinSamples-1.3.2.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Docs-1.3.2.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Source-1.3.2.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-XSLTPatchDistribution-1.0.4-final-patch_1.exe
+
