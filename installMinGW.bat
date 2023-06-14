fd54fdfbf01e18ae7aa76df489a319edd828194a
lothar <lothar>
2023-06-14 23:40:54 +0000

    Adopted setup to support custom MinGW (repackaged jenkins build system). Made attempts to fix issues with mkmk when built with MinGW. Needs a test with the most current MinGW compiler.

--- installMinGW.bat
+++ installMinGW.bat
@@ -3,10 +3,10 @@
 Develop\Tools\MinGW\bin\mingw-get install mingw
 Develop\Tools\MinGW\bin\mingw-get install g++
 
-Develop\Tools\MinGW\bin\mingw-get upgrade "gcc=%1"
-Develop\Tools\MinGW\bin\mingw-get upgrade "g++=%1"
+REM Develop\Tools\MinGW\bin\mingw-get upgrade "gcc=%1"
+REM Develop\Tools\MinGW\bin\mingw-get upgrade "g++=%1"
 
-Develop\Tools\MinGW\bin\mingw-get upgrade "mingw32-w32api=%2"
+REM Develop\Tools\MinGW\bin\mingw-get upgrade "mingw32-w32api=%2"
 
 @rem Required to compile ACE
-Develop\Tools\MinGW\bin\mingw-get install msys
+REM Develop\Tools\MinGW\bin\mingw-get install msys
