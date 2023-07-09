ad0b1945edd91aa9b9ba088e61413b5528a22d41
lothar <lothar>
2023-07-09 12:22:39 +0000

    Non MINGW version without regex

--- vendor/mkmk/stringtools.cpp
+++ vendor/mkmk/stringtools.cpp
@@ -1,12 +1,12 @@
 #include "stringtools.h"
 #include <string>
 
-#ifdef defined(__MINGW32__) || defined(__WATCOMC__) 
+#if defined(__MINGW32__) || defined(__WATCOMC__) 
 #include <regex>
 #endif
 
 char* replaceStringAndDup(char* to, const char* match, const char* replace) {
-#ifdef defined(__MINGW32__) || defined(__WATCOMC__) 
+#if defined(__MINGW32__) || defined(__WATCOMC__) 
 	std::string toReplace = to;
 	std::regex_replace(toReplace, std::regex(match), replace);
 #else
