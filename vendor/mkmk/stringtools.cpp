1f54366358a80368ac8ccaa687b718df9d771e23
lothar <lothar>
2023-07-09 11:41:00 +0000

    Non MINGW version without regex

--- vendor/mkmk/stringtools.cpp
+++ vendor/mkmk/stringtools.cpp
@@ -3,8 +3,13 @@
 #include <regex>
 
 char* replaceStringAndDup(char* to, const char* match, const char* replace) {
+#ifdef __WATCOM__ || __MINGW__
 		std::string toReplace = to;
 		std::regex_replace(toReplace, std::regex(match), replace);
+#else
+		std::string toReplace = to;
+		std::replace( toReplace.begin(), toReplace.end(), match, replace); // replace all 'x' to 'y'	
+#endif
 		return strdup(toReplace.c_str());
 }
 
