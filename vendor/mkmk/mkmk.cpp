3d5e2b91a73f245a921580d0170ffb04ab4b852c
lothar <lothar>
2023-09-03 08:20:10 +0000

    Using dosdir.h for Linux/Unix

--- vendor/mkmk/mkmk.cpp
+++ vendor/mkmk/mkmk.cpp
@@ -12,11 +12,14 @@
 /*...sRevision history:0:*/
 /**************************************************************
  * $Locker:  $
- * $Revision: 1.114.2.29 $
+ * $Revision: 1.114.2.30 $
  * $Name:  $
- * $Id: mkmk.cpp,v 1.114.2.29 2023/09/02 14:34:59 lothar Exp $
+ * $Id: mkmk.cpp,v 1.114.2.30 2023/09/03 08:20:10 lothar Exp $
  *
  * $Log: mkmk.cpp,v $
+ * Revision 1.114.2.30  2023/09/03 08:20:10  lothar
+ * Using dosdir.h for Linux/Unix
+ *
  * Revision 1.114.2.29  2023/09/02 14:34:59  lothar
  * More changes for file and directory find functions
  *
@@ -545,7 +548,7 @@
   #define dd_name name
 #endif
 #ifdef UNIX
-  #include <dos.h>
+  #include <dosdir.h>
   #define dd_findfirst(x,y,z) _dos_findfirst(x,z,y)
   #define dd_findnext(x,y) _dos_findnext(y)
   #define dd_ffblk find_t
@@ -2652,7 +2655,7 @@ void ShowHelp(int argc, char *argv[])
 
   fprintf(stderr, "Enhanced by Lothar Behrens (lothar.behrens@lollisoft.de)\n\n");
 
-  fprintf(stderr, "MKMK: makefile generator $Revision: 1.114.2.29 $\n");
+  fprintf(stderr, "MKMK: makefile generator $Revision: 1.114.2.30 $\n");
   fprintf(stderr, "Usage: MKMK lib|exe|dll|so modulname includepath,[includepath,...] file1 [file2 file3...]\n");
 
   fprintf(stderr, "Your parameters are: ");
