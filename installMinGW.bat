8e4751b96506a97208ed5c17932eb19b4fa05fa3
lothar <lothar>
2023-09-30 20:51:43 +0000

    Tricky link in batch file with space

--- installMinGW.bat
+++ installMinGW.bat
@@ -1,7 +1,7 @@
 @REM This batch will download and install MinGW by utilizing a prepackaged
 @REM 32 bit tool chain.
 curl -k -L -o 7z2301-x64.exe https://www.7-zip.org/a/7z2301-x64.exe
-curl -k -L -o MinGW32.7z https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-win32/dwarf/i686-8.1.0-release-win32-dwarf-rt_v6-rev0.7z
+curl -k -L -o MinGW32.7z https://sourceforge.net/projects/mingw-w64/files/Toolchains%%20targetting%%20Win32/Personal%%20Builds/mingw-builds/8.1.0/threads-win32/dwarf/i686-8.1.0-release-win32-dwarf-rt_v6-rev0.7z
 7z2301-x64.exe /S /D="C:\lbDMF\Develop\Projects\bin"
 C:\lbDMF\Develop\Projects\bin\7z x MinGW32.7z -oC:\lbDMF\Develop\Tools
 @REM %DEVLW%\lbDMF\Develop\Tools\MinGW\bin\mingw-get install mingw
