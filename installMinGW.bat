8a11c526530cf27bbbd7900025377221786c54bd
lothar <lothar>
2023-09-30 14:50:28 +0000

    Custom location not yet in path here

--- installMinGW.bat
+++ installMinGW.bat
@@ -3,7 +3,7 @@
 curl -k -L -o 7z2301-x64.exe https://www.7-zip.org/a/7z2301-x64.exe
 curl -k -L -o MinGW32.7z https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-win32/dwarf/i686-8.1.0-release-win32-dwarf-rt_v6-rev0.7z/download
 7z2301-x64.exe /S /D="C:\lbDMF\Develop\Projects\bin"
-7z x MinGW32.7z -oC:\lbDMF\Develop\Tools
+C:\lbDMF\Develop\Projects\bin\7z x MinGW32.7z -oC:\lbDMF\Develop\Tools
 @REM %DEVLW%\lbDMF\Develop\Tools\MinGW\bin\mingw-get install mingw
 @REM %DEVLW%\lbDMF\Develop\Tools\MinGW\bin\mingw-get install g++
 
