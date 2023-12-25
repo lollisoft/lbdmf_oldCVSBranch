691fd41034444df664b6777809846465830db42b
lothar <lothar>
2023-12-25 20:53:05 +0000

    Changed version informations in preparation for release. Windows works well, Mac works most, but some crashes. Linux yet untested.

--- collect_releases.sh
+++ collect_releases.sh
@@ -3,15 +3,14 @@
 
 
 wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinbuildTools-1.0.4.4-final.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinSamples-1.3.2.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Docs-1.3.2.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Source-1.3.2.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-XSLTPatchDistribution-1.0.4-final-patch_1.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinSamples-1.3.3.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Docs-1.3.3.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Source-1.3.3.exe
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SRPMS/lbdmf-1.3.2-1.src.rpm
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/RPMS/i586/lbdmf-1.3.2-1.i586.rpm
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SOURCES/lbdmf-1.3.2.tgz
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SRPMS/lbdmf-1.3.3-1.src.rpm
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/RPMS/i586/lbdmf-1.3.3-1.i586.rpm
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SOURCES/lbdmf-1.3.3.tgz
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_OSX_Release/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.2-i386.dmg.zip
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_OSX_Release/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.3-i386.dmg.zip
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_PPC/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.2-powerpc.dmg.zip
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_PPC/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.3-powerpc.dmg.zip
