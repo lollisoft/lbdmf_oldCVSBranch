53fd1e103fc26f5923be3ab9d12397ede4ef1889
lothar <lothar>
2024-07-11 10:24:08 +0000

    Added missing cvs2cl tool. Changes for collect_releases.sh to new version.

--- collect_releases.sh
+++ collect_releases.sh
@@ -2,15 +2,16 @@
 
 
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinbuildTools-1.0.4.4-final.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinSamples-1.3.3.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Docs-1.3.3.exe
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Source-1.3.3.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinbuildTools-1.3.4-vc.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-BinSamples-1.3.4.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Docs-1.3.4.exe
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_XPVM_Release/ws/Projects/lbdmf/Output/lbDMF-Source-1.3.4.exe
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SRPMS/lbdmf-1.3.3-1.src.rpm
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/RPMS/i586/lbdmf-1.3.3-1.i586.rpm
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SOURCES/lbdmf-1.3.3.tgz
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SRPMS/lbdmf-1.3.4-1.src.rpm
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/RPMS/i586/lbdmf-1.3.4-1.i586.rpm
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_Linux_Release/ws/Projects/lbdmf/dist/SOURCES/lbdmf-1.3.4.tgz
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_OSX_Release/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.3-i386.dmg.zip
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_OSX_Release/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.4-i386.dmg.zip
 
-wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_PPC/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.3-powerpc.dmg.zip
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_PPC/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.4-powerpc.dmg.zip
+wget http://ci-master.behrens.de:8080/job/lbDMF-Release_1_0_4_stable_rc1_branch_OSXArm/ws/Projects/lbdmf/Test/GUI/wxWrapper/lbDMF-1.3.4-arm.dmg.zip
