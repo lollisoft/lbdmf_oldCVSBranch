a122d8b9c92aba55c14740089443feef95a005a5
lothar <lothar>
2024-08-10 09:38:32 +0000

    Changed flow to create dmg image

--- Test/GUI/wxWrapper/postlink-mac.sh
+++ Test/GUI/wxWrapper/postlink-mac.sh
@@ -61,6 +61,41 @@ export ENTITLEMENTS=--entitlements\ Entitlements.plist
 
 rm -rf `find wxWrapper.app -name CVS -print`
 
+mkdir dmgdist
+cp -R wxWrapper.app dmgdist
+
+# Copy stuff
+mkdir dmgdist/toolbarimages
+cp toolbarimages/*.xpm dmgdist/toolbarimages
+cp toolbarimages/*.png dmgdist/toolbarimages
+cp ../../../COPYING dmgdist
+cp ../../../license-bindist.txt dmgdist
+cp ../../../AppDevelopmentDemo/DynamicApp/Doc/ApplicationprototypingDokumentation.pdf dmgdist
+# Copying templates to an accessable place
+cp -R wxWrapper.app/Contents/Resources/XSLT dmgdist
+cp -R wxWrapper.app/Contents/Resources/UMLSamples dmgdist
+mkdir dmgdist/.lbDMF
+cp -R wxWrapper.app/Contents/Resources/*.sql dmgdist/.lbDMF
+
+cat <<EOF >> dmgdist/Readme.txt
+Dear Mac user!
+
+If you start designing your database applications on one platform (either Intel or PPC) and switch over to a later time, then set the switch to prefer database. This let you update the locally cached files.
+If you use a Sqlite database with more than the lbDMF Manager application in it, please copy the db3 file from the package (/Contents/Resources/lbDMF.db3) and put it into the bundle of the other mac.
+
+This is due to the fact I not yet support an universal application and the design data storage is in the Resources directory in the application bundle to ease moving them around.
+
+Otherwise you could use the PostgreSQL database on a different server.
+
+Thanks
+
+Lothar Behrens
+EOF
+
+rm -rf `find dmgdist -name CVS -print`
+
+cd dmgdist
+
 codesign -f -v -s "$DEVELOPERIDAPP" $ENTITLEMENTS $RUNTIMEOPTIONS wxWrapper.app/Contents/Frameworks/lbHook.framework/Versions/A/lbHook
 codesign -f -v -s "$DEVELOPERIDAPP" $ENTITLEMENTS $RUNTIMEOPTIONS wxWrapper.app/Contents/Frameworks/wxJson.framework/Versions/A/wxJson
 codesign -f -v -s "$DEVELOPERIDAPP" $ENTITLEMENTS $RUNTIMEOPTIONS wxWrapper.app/Contents/Frameworks/wxWrapperDLL.framework/Versions/A/wxWrapperDLL
@@ -82,59 +117,9 @@ xcrun notarytool submit "notarization.zip" --keychain-profile wxWrapper --wait
 
 xcrun stapler staple wxWrapper.app
 
-# Creating a new diskimage
+cd ..
 
-hdiutil create -ov -size 200m -volname lbDMF-$VERSION lbDMF-$VERSION-`uname -p`.dmg -fs HFS+
-
-sleep 5
-
-hdiutil attach lbDMF-$VERSION-`uname -p`.dmg && sleep 5 && cp -R wxWrapper.app /Volumes/lbDMF-$VERSION
-
-# Copy stuff
-
-#mkdir /Volumes/lbDMF-$VERSION/`uname -p`
-
-#cp -R wxWrapper.app /Volumes/lbDMF-$VERSION/`uname -p`
-
-mkdir /Volumes/lbDMF-$VERSION/toolbarimages
-cp toolbarimages/*.xpm /Volumes/lbDMF-$VERSION/toolbarimages
-cp toolbarimages/*.png /Volumes/lbDMF-$VERSION/toolbarimages
-
-cp ../../../COPYING /Volumes/lbDMF-$VERSION
-cp ../../../license-bindist.txt /Volumes/lbDMF-$VERSION
-
-cp ../../../AppDevelopmentDemo/DynamicApp/Doc/ApplicationprototypingDokumentation.pdf /Volumes/lbDMF-$VERSION/
-
-# Copying templates to an accessable place
-
-cp -R wxWrapper.app/Contents/Resources/XSLT /Volumes/lbDMF-$VERSION/
-cp -R wxWrapper.app/Contents/Resources/UMLSamples /Volumes/lbDMF-$VERSION/
-
-mkdir /Volumes/lbDMF-$VERSION/.lbDMF
-cp -R wxWrapper.app/Contents/Resources/*.sql /Volumes/lbDMF-$VERSION/.lbDMF
-
-
-cat <<EOF >> /Volumes/lbDMF-$VERSION/Readme.txt
-Dear Mac user!
-
-If you start designing your database applications on one platform (either Intel or PPC) and switch over to a later time, then set the switch to prefer database. This let you update the locally cached files.
-If you use a Sqlite database with more than the lbDMF Manager application in it, please copy the db3 file from the package (/Contents/Resources/lbDMF.db3) and put it into the bundle of the other mac.
-
-This is due to the fact I not yet support an universal application and the design data storage is in the Resources directory in the application bundle to ease moving them around.
-
-Otherwise you could use the PostgreSQL database on a different server.
-
-Thanks
-
-Lothar Behrens
-EOF
-
-rm -rf `find /Volumes/lbDMF-$VERSION -name CVS -print`
-
-#export OLDPWD = `pwd`
-#cd /Volumes/lbDMF-$VERSION
-
-hdiutil detach /Volumes/lbDMF-$VERSION
+hdiutil create -fs HFS+ -srcfolder dmgdist -volname lbDMF-$VERSION lbDMF-$VERSION-`uname -p`.dmg & sleep 5
 
 codesign -f -v -s "$DEVELOPERIDAPP" -i de.lollisoft.wxWrapper.app lbDMF-$VERSION-`uname -p`.dmg
 
